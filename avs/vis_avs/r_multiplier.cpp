ed9a06031eb902680a168574210e87589a6e0ee3
jakob <grandchild@gmx.net>
2020-08-10 21:36:45 +0200

    [gcc] Port asm in r_multiplier.cpp
    Include some minor hints for improvement as comments.

--- avs/vis_avs/r_multiplier.cpp
+++ avs/vis_avs/r_multiplier.cpp
@@ -168,7 +168,7 @@ int C_THISCLASS::render(char visdata[2][2][576], int isBeat, int *framebuffer, i
 {
   if (isBeat&0x80000000) return 0;
 
-	int b,c;
+	int /*b,*/c; // TODO [cleanup]: see below
 	__int64 mask;
 
 	c = w*h;
@@ -178,7 +178,7 @@ int C_THISCLASS::render(char visdata[2][2][576], int isBeat, int *framebuffer, i
 		__asm {
 			mov ebx, framebuffer;
 			mov ecx, c;
-			mov	edx, b;
+			// mov	edx, b; // doesn't do anything. a leftover?
 			lp0:
 			xor eax, eax;
 			dec ecx;
@@ -193,7 +193,26 @@ int C_THISCLASS::render(char visdata[2][2][576], int isBeat, int *framebuffer, i
 			jmp lp0;			
 		}
 #else // _MSC_VER
-		// TODO: Port to GCC asm
+		__asm__ goto (
+			"mov %%ebx, %0\n\t"
+			"mov %%ecx, %1\n"
+			"lp0:\n\t"
+			"xor %%eax, %%eax\n\t"
+			"dec %%ecx\n\t"
+			"test %%ecx, %%ecx\n\t"
+			"jz %l[end]\n\t"
+			"mov %%eax, dword ptr [%%ebx + %%ecx + 4]\n\t"
+			"test %%eax, %%eax\n\t"
+			"jz sk0\n\t"
+			"mov %%eax, 0xFFFFFF\n"
+			"sk0:\n\t"
+			"mov [%%ebx + %%ecx * 4], %%eax\n\t"
+			"jmp lp0\n\t"
+			:/* no outputs */
+			:"m"(framebuffer), "m"(c)
+			:"eax", "ebx", "ecx"
+			:end
+		);
 #endif
 		break;
 	case MD_XS:
@@ -201,7 +220,7 @@ int C_THISCLASS::render(char visdata[2][2][576], int isBeat, int *framebuffer, i
 		__asm {
 			mov ebx, framebuffer;
 			mov ecx, c;
-			mov	edx, b;
+			// mov	edx, b; // doesn't do anything. a leftover?
 			lp9:
 			xor eax, eax;
 			dec ecx;
@@ -209,14 +228,37 @@ int C_THISCLASS::render(char visdata[2][2][576], int isBeat, int *framebuffer, i
 			jz end;
 			mov eax, dword ptr [ebx+ecx*4];
 			cmp eax, 0xFFFFFF;
-			je sk9;
+			je sk9; // TODO [bugfix]: this could be jae, to account for garbage in MSByte
+			// TODO [improvement]: the next _3_ lines could be compressed into just
+			//         mov [ebx+ecx*4], 0x000000
+			//         sk9:
+			//       since the white pixel (= 0xFFFFFF) doesn't need to be rewritten
 			mov eax, 0x000000;
 			sk9:
 			mov [ebx+ecx*4], eax;
 			jmp lp9;			
 		}
 #else // _MSC_VER
-		// TODO: Port to GCC asm
+		__asm__ goto (
+			"mov %%ebx, %0\n\t"
+			"mov %%ecx, %1\n"
+			"lp9:\n\t"
+			"xor %%eax, %%eax\n\t"
+			"dec %%ecx\n\t"
+			"test %%ecx, %%ecx\n\t"
+			"jz %l[end]\n\t"
+			"mov %%eax, dword ptr [%%ebx + %%ecx + 4]\n\t"
+			"cmp %%eax, 0xFFFFFF\n\t"
+			"je sk9\n\t"
+			"mov %%eax, 0x000000\n"
+			"sk9:\n\t"
+			"mov [%%ebx + %%ecx * 4], %%eax\n\t"
+			"jmp lp9\n\t"
+			:/* no outputs */
+			:"m"(framebuffer), "m"(c)
+			:"eax", "ebx", "ecx"
+			:end
+		);
 #endif
 		break;
 	case MD_X8:
@@ -238,7 +280,25 @@ int C_THISCLASS::render(char visdata[2][2][576], int isBeat, int *framebuffer, i
 			jmp lp1;
 		}
 #else // _MSC_VER
-		// TODO: Port to GCC asm
+		__asm__ goto (
+			"mov %%ebx, %0\n\t"
+			"mov %%ecx, %1\n"
+			"lp1:\n\t"
+			"movq %%mm0, [%%ebx]\n\t"
+			"paddusb %%mm0, %%mm0\n\t"
+			"paddusb %%mm0, %%mm0\n\t"
+			"paddusb %%mm0, %%mm0\n\t"
+			"movq [%%ebx], %%mm0\n\t"
+			"add %%ebx, 8\n\t"
+			"dec %%ecx\n\t"
+			"test %%ecx, %%ecx\n\t"
+			"jz %l[end]\n\t"
+			"jmp lp1\n\t"
+			:/* no outputs */
+			:"m"(framebuffer), "m"(c)
+			:"ebx", "ecx"
+ 			:end
+		);
 #endif
 		break;
 	case MD_X4:
@@ -259,7 +319,24 @@ int C_THISCLASS::render(char visdata[2][2][576], int isBeat, int *framebuffer, i
 			jmp lp2;
 		}
 #else // _MSC_VER
-		// TODO: Port to GCC asm
+		__asm__ goto (
+			"mov %%ebx, %0\n\t"
+			"mov %%ecx, %1\n"
+			"lp2:\n\t"
+			"movq %%mm0, [%%ebx]\n\t"
+			"paddusb %%mm0, %%mm0\n\t"
+			"paddusb %%mm0, %%mm0\n\t"
+			"movq [%%ebx], %%mm0\n\t"
+			"add %%ebx, 8\n\t"
+			"dec %%ecx\n\t"
+			"test %%ecx, %%ecx\n\t"
+			"jz %l[end]\n\t"
+			"jmp lp2\n\t"
+			:/* no outputs */
+			:"m"(framebuffer), "m"(c)
+			:"ebx", "ecx"
+			:end
+		);
 #endif
 		break;
 	case MD_X2:
@@ -279,7 +356,23 @@ int C_THISCLASS::render(char visdata[2][2][576], int isBeat, int *framebuffer, i
 			jmp lp3;
 		}
 #else // _MSC_VER
-		// TODO: Port to GCC asm
+		__asm__ goto (
+			"mov %%ebx, %0\n\t"
+			"mov %%ecx, %1\n"
+			"lp3:\n\t"
+			"movq %%mm0, [%%ebx]\n\t"
+			"paddusb %%mm0, %%mm0\n\t"
+			"movq [%%ebx], %%mm0\n\t"
+			"add %%ebx, 8\n\t"
+			"dec %%ecx\n\t"
+			"test %%ecx, %%ecx\n\t"
+			"jz %l[end]\n\t"
+			"jmp lp3\n\t"
+			:/* no outputs */
+			:"m"(framebuffer), "m"(c)
+			:"ebx", "ecx"
+			:end
+		);
 #endif
 		break;
 	case MD_X05:
@@ -302,7 +395,25 @@ int C_THISCLASS::render(char visdata[2][2][576], int isBeat, int *framebuffer, i
 			jmp lp4;
 		}
 #else // _MSC_VER
-		// TODO: Port to GCC asm
+		__asm__ goto (
+			"mov %%ebx, %0\n\t"
+			"mov %%ecx, %1\n\t"
+			"movq %%mm1, %2\n"
+			"lp4:\n\t"
+			"movq %%mm0, [%%ebx]\n\t"
+			"psrlq %%mm0, 1\n\t"
+			"pand %%mm0, %%mm1\n\t"
+			"movq [%%ebx], %%mm0\n\t"
+			"add %%ebx, 8\n\t"
+			"dec %%ecx\n\t"
+			"test %%ecx, %%ecx\n\t"
+			"jz %l[end]\n\t"
+			"jmp lp4\n\t"
+			:/* no outputs */
+			:"m"(framebuffer), "m"(c), "m"(mask)
+			:"ebx", "ecx"
+			:end
+		);
 #endif
 		break;
 	case MD_X025:
@@ -325,7 +436,25 @@ int C_THISCLASS::render(char visdata[2][2][576], int isBeat, int *framebuffer, i
 			jmp lp5;
 		}
 #else // _MSC_VER
-		// TODO: Port to GCC asm
+		__asm__ goto (
+			"mov %%ebx, %0\n\t"
+			"mov %%ecx, %1\n\t"
+			"movq %%mm1, %2\n"
+			"lp5:\n\t"
+			"movq %%mm0, [%%ebx]\n\t"
+			"psrlq %%mm0, 2\n\t"
+			"pand %%mm0, %%mm1\n\t"
+			"movq [%%ebx], %%mm0\n\t"
+			"add %%ebx, 8\n\t"
+			"dec %%ecx\n\t"
+			"test %%ecx, %%ecx\n\t"
+			"jz %l[end]\n\t"
+			"jmp lp5\n\t"
+			:/* no outputs */
+			:"m"(framebuffer), "m"(c), "m"(mask)
+			:"ebx", "ecx"
+			:end
+		);
 #endif
 		break;
 	case MD_X0125:
@@ -348,7 +477,25 @@ int C_THISCLASS::render(char visdata[2][2][576], int isBeat, int *framebuffer, i
 			jmp lp6;
 		}
 #else // _MSC_VER
-		// TODO: Port to GCC asm
+		__asm__ goto (
+			"mov %%ebx, %0\n\t"
+			"mov %%ecx, %1\n\t"
+			"movq %%mm1, %2\n"
+			"lp6:\n\t"
+			"movq %%mm0, [%%ebx]\n\t"
+			"psrlq %%mm0, 3\n\t"
+			"pand %%mm0, %%mm1\n\t"
+			"movq [%%ebx], %%mm0\n\t"
+			"add %%ebx, 8\n\t"
+			"dec %%ecx\n\t"
+			"test %%ecx, %%ecx\n\t"
+			"jz %l[end]\n\t"
+			"jmp lp6\n\t"
+			:/* no outputs */
+			:"m"(framebuffer), "m"(c), "m"(mask)
+			:"ebx", "ecx"
+			:end
+		);
 #endif
 		break;
 	}
@@ -356,7 +503,7 @@ int C_THISCLASS::render(char visdata[2][2][576], int isBeat, int *framebuffer, i
 #ifdef _MSC_VER
 	__asm emms;
 #else // _MSC_VER
-	// TODO: Port to GCC asm
+	__asm__ __volatile__ ("emms");
 #endif
 	return 0;
 }
