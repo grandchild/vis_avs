db96fa7e4163dcaba130b16fef3961cfb742a9fe
jakob <grandchild@gmx.net>
2021-02-18 23:09:31 +0100

    [doc] Indent and clean build.sh some more

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
+#!/usr/bin/env sh
 
 if [ ! -d build ]; then
-    (rm -rf build && mkdir build && cd build && i686-w64-mingw32-cmake -D CMAKE_TOOLCHAIN_FILE=../CMake-MingWcross-toolchain.txt .. && make)
+    (mkdir -p build \
+        && cd build \
+        && i686-w64-mingw32-cmake \
+            -D CMAKE_TOOLCHAIN_FILE=../CMake-MingWcross-toolchain.txt \
+            .. \
+        && make
+    )
 fi
 
 (cd build \
