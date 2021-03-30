50236d55f4c4f9ae746cb06da26d87f03191abe0
jakob <grandchild@gmx.net>
2021-03-30 18:41:35 +0200

    [gcc] Add missing stdio include in nseel-compiler.c
    Only needed for one printf of a very unlikely UD2-padding error.

--- avs/ns-eel/nseel-compiler.c
+++ avs/ns-eel/nseel-compiler.c
@@ -29,6 +29,7 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include <windows.h>
+#include <stdio.h>
 #include "ns-eel-int.h"
 
 #ifdef NSEEL_REENTRANT_EXECUTION
