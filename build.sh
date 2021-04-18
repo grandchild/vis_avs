5d57e51eabb0427804a327cb265c6c5928b1c670
exo-cortex <lukipuki@physik.tu-berlin.de>
2021-04-18 15:23:27 +0200

    [doc] comment and explain ape tutorial file.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 if [ ! -d build ]; then
     (mkdir -p build \
         && cd build \
-        && i686-w64-mingw32-cmake \
+        && mingw32-cmake \
             -D CMAKE_TOOLCHAIN_FILE=../CMake-MingWcross-toolchain.txt \
             .. \
         && make
