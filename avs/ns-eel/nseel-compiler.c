grandchild/vis_avs

commit 7f30aed886b7e3c1205cf646383945bbb8c46ede

[msvc] Fix function copying in EEL compiler
Function pointers, in the way they were used by the EEL compiler,
aren't working in MSVC.

MSVC moves functions around at will, so it's not guaranteed that any
function foo() will be immediately followed by its foo_end() marker
function (which is used to compute the function's size) in the binary.

Take a hint from GCC and explicitly put a marker in the form of two
UD2 instructions at the end of every function. On MSVC, use that to
compute function size, instead of the *_end() function pointers.
Note that MSVC pads functions with int3 interrupts, instead of UD2,
like GCC.

Additionally, in a debug build, MSVC puts function addresses into a
jump table, which breaks direct function binary copy by pointer
lookup.

Change function lookup to check for a jump instruction at the address,
decoding it and returning the jump target as the real address.

On MSVC a function in the binary now looks like this in the worst
case:

    0003  jmp 0x00000100        ; Jump table entry for foo()
    0008  jmp 0x00000090        ; Jump table entry for foo_end()
        ...
        ...
    0090  int3                  ; Start of foo_end(), only padding
                                ; We don't use this at all on MSVC
                                ; anymore.
        ...
    0100  mov ebp, esp          ; Start of foo()
        ...                     ; foo() body
    011a  ud2                   ; foo() end marker
    011c  ud2
    011e  int3                  ; foo() padding
    011f  int3
    0120  mov ebp, esp          ; Start of bar()
        ...

Change the EEL API for NSEEL_addfunction() and NSEEL_addfunctionex()
from taking start pointer and size of a function, to function start
and end pointers instead. This prevents the API-using code from having
to deal with the jump table, and as a bonus, simplifies the code.

avs/ns-eel/nseel-compiler.c
@@ -193,7 +193,7 @@ int NSEEL_init() // returns 0 on success
 /* Register a function by name, with number of parameters, code start address, length of
    the code section in bytes, and optional preprocessing function. Pass 0 for pproc to
    skip preprocessing. */
-void NSEEL_addfunctionex(char *name, int nparms, int code_startaddr, int code_len, NSEEL_PPPROC pproc)
+void NSEEL_addfunctionex(char *name, int nparms, void(*code_startaddr)(), void(*code_endaddr)(), NSEEL_PPPROC pproc)
 {
   if (!fnTableUser || !(fnTableUser_size&7))
   {
@@ -203,8 +203,8 @@ void NSEEL_addfunctionex(char *name, int nparms, int code_startaddr, int code_le
   {
     fnTableUser[fnTableUser_size].nParams = nparms;
     fnTableUser[fnTableUser_size].name = name;
-    fnTableUser[fnTableUser_size].afunc = (void *)code_startaddr;
-    fnTableUser[fnTableUser_size].func_e = (void *)(code_startaddr + code_len);
+    fnTableUser[fnTableUser_size].afunc = code_startaddr;
+    fnTableUser[fnTableUser_size].func_e = code_endaddr;
     fnTableUser[fnTableUser_size].pProc = (NSEEL_PPPROC) pproc;
     fnTableUser_size++;
   }
@@ -215,25 +215,6 @@ void NSEEL_quit()
   free(fnTableUser);
   fnTableUser_size=0;
   fnTableUser=0;
-}
-
-//---------------------------------------------------------------------------------------------------------------
-static void *realAddress(void *fn, void *fn_e, int *size)
-{
-#ifdef DISABLED_DEBUG
-  char *ptr = (char *)fn;
-  int beg=(*(int *)(ptr+1))+5;
-
-  int extrasize=(int)nseel_asm_exec2_end - (int)nseel_asm_exec2;
-  int extrabeg=(*(int *)(((char *)nseel_asm_exec2)+1))+5;
-
-  *size=((int)fn_e - (int)fn) - (extrasize-extrabeg) - beg;
-  return ptr + beg;
-#else
-  // Release Mode
-  *size  = (int)fn_e - (int) fn;
-  return fn;
-#endif
 }
 
 //---------------------------------------------------------------------------------------------------------------
@@ -303,10 +284,83 @@ static void *__newBlock(llBlock **start, int size)
 #define X86_RET 0xC3
 
 #define X86_NOP 0x90
-#define X86_NEAR_JMP 0xEB
-#define SIZE_X86_NEAR_JMP_BYTES 2
+#define X86_SHORT_JMP 0xEB
+#define X86_NEAR_JMP 0xE9
+#define SIZE_X86_NEAR_JMP (1 /*opcode*/ + 4 /*address*/)
+#define X86_INT3 0xCC
+#define SIZE_X86_SHORT_JMP_BYTES 2
 #define UD2_FIRST_BYTE 0x0F
 #define UD2_SECOND_BYTE 0x0B
+
+//---------------------------------------------------------------------------------------------------------------
+#ifdef _MSC_VER
+/* For debug builds MSVC puts functions into a jump table, and since we want the actual
+   code block for the compiled function we have to follow that JMP instruction. The
+   relative address is encoded in the 4 bytes after the 1 byte opcode. */
+void* resolve_msvc_jumptable_function_address(void* fn) {
+    unsigned char* c_fn = (char*)fn;
+    if (*c_fn != X86_NEAR_JMP) {
+        return fn;
+    }
+    return (unsigned int)fn + SIZE_X86_NEAR_JMP + (*(int*)&c_fn[1]);
+}
+
+/* MSVC padds naked functions with INT3 breakpoint traps. But we need to execute several
+   bits of code made from naked functions one after the other. So we need the actual end
+   of the function. Since MSVC relocates functions at will, the "*_end" function
+   pointers which work for GCC aren't reliable. */
+
+#define MAX_FUNCTION_SIZE 1024  // This is a high number, functions are much smaller.
+
+/* Find the length in bytes of the function by searching forward for a double UD2
+   instruction placed at the end of all functions through the _MARK_FUNCTION_END macro
+   (see ns-eel-addfuncs.h).
+
+   NOTE: It's conceivable, but unlikely, that the function code may contain the bytes
+   0F-0B-0F-0B (two times UD2). In that case the size detection would fail, with
+   unpredictable results. */
+static int find_msvc_naked_function_size(unsigned char* fn) {
+    char match_state = 0;
+    for (unsigned char* p = fn; p < fn + MAX_FUNCTION_SIZE; p++) {
+        if (*p == UD2_FIRST_BYTE && (match_state == 0 || match_state == 2)) {
+            match_state++;
+        } else if (*p == UD2_SECOND_BYTE) {
+            if (match_state == 1) {
+                match_state++;
+            } else if (match_state == 3) {
+                return p - fn - 3;
+            } else {
+                match_state = 0;
+            }
+        } else {
+            match_state = 0;
+        }
+    }
+}
+#endif
+
+static void* realAddress(void* fn, void* fn_e, int* size)
+{
+#ifdef DISABLED_DEBUG
+    char* ptr = (char*)fn;
+    int beg = (*(int*)(ptr + 1)) + 5;
+
+    int extrasize = (int)nseel_asm_exec2_end - (int)nseel_asm_exec2;
+    int extrabeg = (*(int*)(((char*)nseel_asm_exec2) + 1)) + 5;
+
+    *size = ((int)fn_e - (int)fn) - (extrasize - extrabeg) - beg;
+    return ptr + beg;
+#else
+    // Release Mode
+#ifdef _MSC_VER
+    fn = resolve_msvc_jumptable_function_address(fn);
+    *size = find_msvc_naked_function_size(fn);
+#else
+    *size = (int)fn_e - (int)fn;
+#endif
+    return fn;
+#endif
+}
 
 //---------------------------------------------------------------------------------------------------------------
 static int *findFBlock(char *p)
@@ -346,9 +400,9 @@ void write_gcc_naked_function_trap_padding_jmp(unsigned char* code, int size2) {
       // TODO: better error handling here. Code generation should fail!
       return;
     } else if(epilog_size > 2) {
-      code[size2 - epilog_size] = X86_NEAR_JMP;
+      code[size2 - epilog_size] = X86_SHORT_JMP;
       code[size2 - epilog_size + 1] = (unsigned char)epilog_size
-                                      - SIZE_X86_NEAR_JMP_BYTES;
+                                      - SIZE_X86_SHORT_JMP_BYTES;
     } else if(epilog_size == 2) {
       code[size2 - epilog_size] = X86_NOP;
       code[size2 - epilog_size + 1] = X86_NOP;
@@ -436,7 +490,7 @@ int nseel_createCompiledFunction3(compileContext *ctx, int fntype, int fn, int c
 
   if (fntype == MATH_FN && fn == 0) // special case: IF
   {
-    void *func3;
+    int func3;
     int size;
     int *ptr;
     char *block;
@@ -453,13 +507,14 @@ int nseel_createCompiledFunction3(compileContext *ctx, int fntype, int fn, int c
 
     ctx->l_stats[2]+=sizes2+sizes3+2;
     
-    func3 = realAddress(nseel_asm_if,nseel_asm_if_end,&size);
+    NSEEL_PPPROC preProc;
+    func3 = nseel_getFunctionAddress(fntype, fn, &size, &preProc);
 
     block=(char *)newTmpBlock(4+sizes1+size);
     ((int*)block)[0]=sizes1+size;
     memcpy(block+4,(char*)code1+4,sizes1);
     ptr=(int *)(block+4+sizes1);
-    memcpy(ptr,func3,size);
+    memcpy(ptr,(void*)func3,size);
 #ifdef __GNUC__
     write_gcc_naked_function_trap_padding_jmp((unsigned char*)ptr, size);
 #endif
@@ -524,7 +579,7 @@ int nseel_createCompiledFunction2(compileContext *ctx, int fntype, int fn, int c
 #ifdef NSEEL_LOOPFUNC_SUPPORT
   if (fntype == MATH_FN && fn == 1) // special case: REPEAT
   {
-    void *func3;
+    int func3;
     int size;
     int *ptr;
     char *block;
@@ -537,13 +592,13 @@ int nseel_createCompiledFunction2(compileContext *ctx, int fntype, int fn, int c
 
     ctx->l_stats[2]+=sizes2+2;
     
-    func3 = realAddress(nseel_asm_repeat,nseel_asm_repeat_end,&size);
+    func3 = nseel_getFunctionAddress(fntype, fn, &size, NULL);
 
     block=(char *)newTmpBlock(4+sizes1+size);
     ((int*)block)[0]=sizes1+size;
     memcpy(block+4,(char*)code1+4,sizes1);
     ptr=(int *)(block+4+sizes1);
-    memcpy(ptr,func3,size);
+    memcpy(ptr,(void*)func3,size);
 #ifdef __GNUC__
     write_gcc_naked_function_trap_padding_jmp((unsigned char*)ptr, size);
 #endif
@@ -849,6 +904,7 @@ void NSEEL_code_execute(NSEEL_CODEHANDLE code)
       and ebx, ~31
       mov edi, ebx
       call eax
+
       popad
     }
 #else

avs/ns-eel/megabuf.c
@@ -116,6 +116,7 @@ NAKED void _asm_megabuf(void)
 
   __asm { mov eax, __nextBlock } // this is custom, returning pointer
   __asm { mov esp, ebp }
+  _MARK_FUNCTION_END
 #else
   __asm__ __volatile__ (
     "mov  %%edx, 0xffffffff\n\t"        // placeholder for blocks pointer (replaced

avs/ns-eel/ns-eel-addfuncs.h
@@ -91,10 +91,15 @@ be sure to preserve edi, too.
 #define CALL2_PFASTCALL(FUNC) _ARGS2; *__nextBlock = __##FUNC(parm_a, parm_b); _LEAVE
 #define CALL3_PFASTCALL(FUNC) _ARGS3; *__nextBlock = __##FUNC(parm_a, parm_b, parm_c); _LEAVE
 
+#define _MARK_FUNCTION_END \
+  __asm { UD2 } \
+  __asm { UD2 }
+
 #define _LEAVE \
   __asm { mov eax, esi } \
   __asm { add esi, 8 }  \
-  __asm { mov esp, ebp }
+  __asm { mov esp, ebp } \
+  _MARK_FUNCTION_END
 
 
 #else  // GCC
@@ -178,6 +183,9 @@ be sure to preserve edi, too.
   "mov  %%eax, %%esi\n\t"      \
   "add  %%esi, 8\n\t"          \
   "mov  %%esp, %%ebp\n\t"
+
+// Used only for MSVC, see above
+#define _MARK_FUNCTION_END
 
 #endif
 

avs/ns-eel/ns-eel.h
@@ -39,8 +39,8 @@ extern "C" {
 typedef void (*NSEEL_PPPROC)(void *data, int data_size, void **userfunc_data);
 
 int NSEEL_init(); // returns 0 on success
-#define NSEEL_addfunction(name,nparms,code,len) NSEEL_addfunctionex((name),(nparms),(code),(len),0)
-void NSEEL_addfunctionex(char *name, int nparms, int code_startaddr, int code_len, NSEEL_PPPROC pproc);
+#define NSEEL_addfunction(name,nparms,code_start,code_end) NSEEL_addfunctionex((name),(nparms),(code_start),(code_end),NULL)
+void NSEEL_addfunctionex(char *name, int nparms, void(*code_startaddr)(), void(*code_endaddr)(), NSEEL_PPPROC pproc);
 void NSEEL_quit();
 int *NSEEL_getstats(); // returns a pointer to 5 ints... source bytes, static code bytes, call code bytes, data bytes, number of code handles
 double *NSEEL_getglobalregs();

avs/ns-eel/nseel-cfunc.c
@@ -212,6 +212,7 @@ NAKED void nseel_asm_ceil_end(void) {}
 // do nothing, eh
 NAKED void nseel_asm_exec2(void)
 {
+    _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_exec2_end(void) { }
 
@@ -275,6 +276,7 @@ NAKED void nseel_asm_invsqrt(void)
     :"eax", "edx", "esi", "ecx"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_invsqrt_end(void) {}
 
@@ -301,6 +303,7 @@ NAKED void nseel_asm_sin(void)
     : : :"eax", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_sin_end(void) {}
 
@@ -326,6 +329,7 @@ NAKED void nseel_asm_cos(void)
     : : :"eax", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_cos_end(void) {}
 
@@ -353,6 +357,7 @@ NAKED void nseel_asm_tan(void)
     : : : "eax", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_tan_end(void) {}
 
@@ -378,6 +383,7 @@ NAKED void nseel_asm_sqr(void)
     : : : "eax", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_sqr_end(void) {}
 
@@ -405,6 +411,7 @@ NAKED void nseel_asm_sqrt(void)
     : : : "eax", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_sqrt_end(void) {}
 
@@ -437,6 +444,7 @@ NAKED void nseel_asm_log(void)
     : : : "eax", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_log_end(void) {}
 
@@ -468,6 +476,7 @@ NAKED void nseel_asm_log10(void)
     : : : "eax", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_log10_end(void) {}
 
@@ -493,6 +502,7 @@ NAKED void nseel_asm_abs(void)
     : : : "eax", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_abs_end(void) {}
 
@@ -513,6 +523,7 @@ NAKED void nseel_asm_assign(void)
     : : : "eax", "ebx"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_assign_end(void) {}
 
@@ -538,6 +549,7 @@ NAKED void nseel_asm_add(void)
     : : : "eax", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_add_end(void) {}
 
@@ -563,6 +575,7 @@ NAKED void nseel_asm_sub(void)
     : : : "eax", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_sub_end(void) {}
 
@@ -588,6 +601,7 @@ NAKED void nseel_asm_mul(void)
     : : : "eax", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_mul_end(void) {}
 
@@ -613,6 +627,7 @@ NAKED void nseel_asm_div(void)
     : : : "eax", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_div_end(void) {}
 
@@ -670,6 +685,7 @@ NAKED void nseel_asm_mod(void)
     : "eax", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_mod_end(void) {}
 
@@ -709,6 +725,7 @@ NAKED void nseel_asm_or(void)
     : : : "eax", "ebx", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_or_end(void) {}
 
@@ -750,6 +767,7 @@ NAKED void nseel_asm_and(void)
     : : : "eax", "ebx", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_and_end(void) {}
 
@@ -769,6 +787,7 @@ NAKED void nseel_asm_uplus(void) // this is the same as doing nothing, it seems
     mov nextBlock, ebx
   }
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_uplus_end(void) {}
 
@@ -798,6 +817,7 @@ NAKED void nseel_asm_uminus(void)
     : : : "eax", "ebx", "ecx", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_uminus_end(void) {}
 
@@ -851,6 +871,7 @@ zero:
     : "eax", "ecx", "edx", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_sign_end(void) {}
 
@@ -890,6 +911,7 @@ NAKED void nseel_asm_bnot(void)
     : "eax", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_bnot_end(void) {}
 
@@ -934,6 +956,7 @@ NAKED void nseel_asm_if(void)
     : "eax", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_if_end(void) {}
 
@@ -991,6 +1014,7 @@ skip:
     :"eax", "ecx", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_repeat_end(void) {}
 #endif
@@ -1031,6 +1055,7 @@ NAKED void nseel_asm_equal(void)
     :"eax", "ebx", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_equal_end(void) {}
 
@@ -1066,6 +1091,7 @@ NAKED void nseel_asm_below(void)
     :"eax", "ebx", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_below_end(void) {}
 
@@ -1102,6 +1128,7 @@ NAKED void nseel_asm_above(void)
     :"eax", "ebx", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_above_end(void) {}
 
@@ -1143,6 +1170,7 @@ NAKED void nseel_asm_min(void)
     :"eax", "ebx", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_min_end(void) {}
 
@@ -1181,6 +1209,7 @@ NAKED void nseel_asm_max(void)
     :"eax", "ebx", "esi"
   );
 #endif
+  _MARK_FUNCTION_END
 }
 NAKED void nseel_asm_max_end(void) {}
 

avs/vis_avs/avs_eelif.cpp
@@ -205,14 +205,14 @@ void AVS_EEL_IF_init()
 {
   InitializeCriticalSection(&g_eval_cs);
   NSEEL_init();
-  NSEEL_addfunction("getosc",3,(int)_asm_getosc,(int)_asm_getosc_end-(int)_asm_getosc);
-  NSEEL_addfunction("getspec",3,(int)_asm_getspec,(int)_asm_getspec_end-(int)_asm_getspec);
-  NSEEL_addfunction("gettime",1,(int)_asm_gettime,(int)_asm_gettime_end-(int)_asm_gettime);
-  NSEEL_addfunction("getkbmouse",1,(int)_asm_getmouse,(int)_asm_getmouse_end-(int)_asm_getmouse);
-  NSEEL_addfunction("setmousepos",2,(int)_asm_setmousepos,(int)_asm_setmousepos_end-(int)_asm_setmousepos);
+  NSEEL_addfunction("getosc",3,_asm_getosc,_asm_getosc_end);
+  NSEEL_addfunction("getspec",3,_asm_getspec,_asm_getspec_end);
+  NSEEL_addfunction("gettime",1,_asm_gettime,_asm_gettime_end);
+  NSEEL_addfunction("getkbmouse",1,_asm_getmouse,_asm_getmouse_end);
+  NSEEL_addfunction("setmousepos",2,_asm_setmousepos,_asm_setmousepos_end);
 #ifdef AVS_MEGABUF_SUPPORT
-    NSEEL_addfunctionex("megabuf",1,(int)_asm_megabuf,(int)_asm_megabuf_end-(int)_asm_megabuf,megabuf_ppproc);
-    NSEEL_addfunction("gmegabuf",1,(int)_asm_gmegabuf,(int)_asm_gmegabuf_end-(int)_asm_gmegabuf);
+    NSEEL_addfunctionex("megabuf",1,_asm_megabuf,_asm_megabuf_end,megabuf_ppproc);
+    NSEEL_addfunction("gmegabuf",1,_asm_gmegabuf,_asm_gmegabuf_end);
 #endif
 }
 void AVS_EEL_IF_quit()
@@ -350,6 +350,7 @@ NAKED void _asm_gmegabuf(void)
 
   __asm { mov eax, __nextBlock } // this is custom, returning pointer
   __asm { mov esp, ebp }
+  _MARK_FUNCTION_END
 #else
   __asm__ __volatile__ (
     "mov  %%ebp, %%esp\n\t"         // new stack frame

commit ad80d78cb43f6c0bad11e96020b6e7510187bfd9

[bug] Fix NULL pproc arg for EEL getFunctionAddress
Passing a NULL pointer was previously not allowed, but is now checked
for and possible if no preprocessing function is needed.

This bug was introduced in
    7f30aed "Fix function copying in EEL compiler"
when "if" and "loop" address lookup was changed to use
nseel_getFunctionAddress() as well.

avs/ns-eel/nseel-compiler.c
@@ -437,7 +437,9 @@ int nseel_createCompiledValue(compileContext *ctx, double value, double *addrVal
 //---------------------------------------------------------------------------------------------------------------
 static int nseel_getFunctionAddress(int fntype, int fn, int *size, NSEEL_PPPROC *pProc)
 {
-  *pProc = NULL;
+  if(pProc) {
+    *pProc = NULL;
+  }
   switch (fntype)
   {
     case MATH_SIMPLE:
@@ -473,7 +475,7 @@ static int nseel_getFunctionAddress(int fntype, int fn, int *size, NSEEL_PPPROC 
           if (size) *size=0;
           return 0;
         }
-        if (p->pProc) *pProc=p->pProc;
+        if (p->pProc && pProc) *pProc=p->pProc;
         return (int)realAddress(p->afunc,p->func_e,size);
       }
   }   
@@ -507,8 +509,7 @@ int nseel_createCompiledFunction3(compileContext *ctx, int fntype, int fn, int c
 
     ctx->l_stats[2]+=sizes2+sizes3+2;
     
-    NSEEL_PPPROC preProc;
-    func3 = nseel_getFunctionAddress(fntype, fn, &size, &preProc);
+    func3 = nseel_getFunctionAddress(fntype, fn, &size, NULL);
 
     block=(char *)newTmpBlock(4+sizes1+size);
     ((int*)block)[0]=sizes1+size;