9a234cb7f7383e88a751509da2d78802646c0080
jakob <grandchild@gmx.net>
2021-10-15 23:58:44 +0200

    [clean] Remove APEinfo struct in TexerII
    It's provided in ape.h, simply include this like everywhere else. The
    explicit (re)definition was a leftover from the APE->builtin import.

--- avs/vis_avs/r_texer2.h
+++ avs/vis_avs/r_texer2.h
@@ -2,48 +2,7 @@
 
 #include "c_texer2.h"
 
-
-typedef struct
-{
-    int ver; // ver=1 to start
-    double *global_registers; // 100 of these
-
-    // lineblendmode: 0xbbccdd
-    // bb is line width (minimum 1)
-    // dd is blend mode:
-        // 0=replace
-        // 1=add
-        // 2=max
-        // 3=avg
-        // 4=subtractive (1-2)
-        // 5=subtractive (2-1)
-        // 6=multiplicative
-        // 7=adjustable (cc=blend ratio)
-        // 8=xor
-        // 9=minimum
-    int *lineblendmode;
-
-    //evallib interface
-    VM_CONTEXT (*allocVM)(); // return a handle
-    void (*freeVM)(VM_CONTEXT); // free when done with a VM and ALL of its code have been freed, as well
-
-    // you should only use these when no code handles are around (i.e. it's okay to use these before
-    // compiling code, or right before you are going to recompile your code.
-    void (*resetVM)(VM_CONTEXT);
-    double * (*regVMvariable)(VM_CONTEXT, char *name);
-
-    // compile code to a handle
-    VM_CODEHANDLE (*compileVMcode)(VM_CONTEXT, char *code);
-
-    // execute code from a handle
-    void (*executeCode)(VM_CODEHANDLE, char visdata[2][2][576]);
-
-    // free a code block
-    void (*freeCode)(VM_CODEHANDLE);
-
-    // requires ver >= 2
-    void (*doscripthelp)(HWND hwndDlg,char *mytext); // mytext can be NULL for no custom page
-} APEinfo;
+#include "ape.h"
 
 
 #ifdef _MSC_VER
