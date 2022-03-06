7dbd589a34eaaf676fd26b49d2522593f36a8c9d
jakob <grandchild@gmx.net>
2022-03-06 11:36:43 +0100

    [port][effect] New-style Water

--- avs/vis_avs/g_water.cpp
+++ avs/vis_avs/g_water.cpp
@@ -1,4 +1,4 @@
-#include "c_water.h"
+#include "e_water.h"
 
 #include "g__defs.h"
 #include "g__lib.h"
@@ -8,20 +8,16 @@
 #include <windows.h>
 
 int win32_dlgproc_water(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM) {
-    C_THISCLASS* g_this = (C_THISCLASS*)g_current_render;
+    E_Water* g_this = (E_Water*)g_current_render;
 
     switch (uMsg) {
         case WM_INITDIALOG:
-            if (g_this->enabled) CheckDlgButton(hwndDlg, IDC_CHECK1, BST_CHECKED);
+            CheckDlgButton(hwndDlg, IDC_CHECK1, g_this->enabled);
             return 1;
         case WM_COMMAND:
             if (LOWORD(wParam) == IDC_CHECK1) {
-                if (IsDlgButtonChecked(hwndDlg, IDC_CHECK1))
-                    g_this->enabled = 1;
-                else
-                    g_this->enabled = 0;
+                g_this->enabled = IsDlgButtonChecked(hwndDlg, IDC_CHECK1);
             }
-            return 0;
     }
     return 0;
 }
