1b4620bc4e22b9e106f8ca39e9b882c3c7f73a24
jakob <grandchild@gmx.net>
2022-03-21 19:56:23 +0100

    [port] Add unsigned min()/max() helper functions

--- avs/platform.c
+++ avs/platform.c
@@ -2,3 +2,5 @@
 
 int min(int a, int b) { return a > b ? b : a; }
 int max(int a, int b) { return a < b ? b : a; }
+unsigned int umin(unsigned int a, unsigned int b) { return a > b ? b : a; }
+unsigned int umax(unsigned int a, unsigned int b) { return a < b ? b : a; }
