72ba4ace9aa80ea415a8ba47f234ce4ea079bceb
jakob <grandchild@gmx.net>
2022-04-02 01:56:42 +0200

    [port][effect] New-style AVI
    Use the new `AVS_Video` for playback. Support many more video formats
    instead of just AVI.
    
    Add a resampling setting to the UI. This is a departure from the
    strict compatibility of the old AVS. Cannot be saved at the moment
    because the save format is unchanged.
    
    Remove video-for-windows (vfw) include and linking.

---
+++ avs/vis_avs/e_avi.h
@@ -0,0 +1,100 @@
+#pragma once
+
+#include "effect.h"
+#include "effect_info.h"
+#include "video.h"
+
+#include "../platform.h"
+
+enum AVI_Blend_Modes {
+    AVI_BLEND_REPLACE = 0,
+    AVI_BLEND_ADD = 1,
+    AVI_BLEND_FIFTY_FIFTY = 2,
+    AVI_BLEND_FIFTY_FIFTY_ONBEAT_ADD = 3,
+};
+
+struct AVI_Config : public Effect_Config {
+    int64_t filename = -1;
+    int64_t blend_mode = AVI_BLEND_FIFTY_FIFTY;
+    int64_t on_beat_persist = 6;
+    int64_t playback_speed = 0;
+    int64_t resampling_mode = AVS_Video::SCALE_BILINEAR;
+};
+
+struct AVI_Info : public Effect_Info {
+    static constexpr char* group = "Render";
+    static constexpr char* name = "AVI";
+    static constexpr char* help = "";
+    static constexpr int32_t legacy_id = 32;
+    static constexpr char* legacy_ape_id = NULL;
+
+    static const char* const* video_files(int64_t* length_out);
+    static const char* const* blend_modes(int64_t* length_out) {
+        *length_out = 4;
+        static const char* const options[4] = {
+            "Replace",
+            "Additive",
+            "50/50",
+            "50/50 + On-Beat Additive",
+        };
+        return options;
+    };
+    static const char* const* resampling_modes(int64_t* length_out) {
+        *length_out = 5;
+        static const char* const options[5] = {
+            "Nearest Neighbor",
+            "Fast Bilinear",
+            "Bilinear",
+            "Bicubic",
+            "Spline",
+        };
+        return options;
+    };
+
+    static void on_file_change(Effect*, const Parameter*, std::vector<int64_t>);
+
+    static constexpr uint32_t num_parameters = 5;
+    static constexpr Parameter parameters[num_parameters] = {
+        P_SELECT(offsetof(AVI_Config, filename),
+                 "Filename",
+                 video_files,
+                 NULL,
+                 on_file_change),
+        P_SELECT(offsetof(AVI_Config, blend_mode), "Blend Mode", blend_modes),
+        P_IRANGE(offsetof(AVI_Config, on_beat_persist), "On-Beat Persistance", 0, 32),
+        P_IRANGE(offsetof(AVI_Config, playback_speed), "Playback Speed", 0, 1000),
+        P_SELECT(offsetof(AVI_Config, resampling_mode),
+                 "Resampling Mode",
+                 resampling_modes),
+    };
+
+    EFFECT_INFO_GETTERS;
+};
+
+class E_AVI : public Configurable_Effect<AVI_Info, AVI_Config> {
+   public:
+    E_AVI();
+    virtual ~E_AVI();
+    virtual int render(char visdata[2][2][576],
+                       int isBeat,
+                       int* framebuffer,
+                       int* fbout,
+                       int w,
+                       int h);
+    virtual void load_legacy(unsigned char* data, int len);
+    virtual int save_legacy(unsigned char* data);
+
+    void find_video_files();
+    void clear_video_files();
+    void load_file();
+    void close_file();
+
+    static std::vector<std::string> filenames;
+    static const char** c_filenames;
+
+    AVS_Video* video;
+    bool loaded;
+    int64_t frame_index;
+    int64_t last_time;
+    lock_t* video_file_lock;
+};
--- avs/vis_avs/g_avi.cpp
+++ avs/vis_avs/g_avi.cpp
@@ -1,4 +1,4 @@
-#include "c_avi.h"
+#include "e_avi.h"
 
 #include "g__defs.h"
 #include "g__lib.h"
@@ -8,65 +8,122 @@
 #include <windows.h>
 #include <commctrl.h>
 
-static void EnableWindows(HWND hwndDlg, C_THISCLASS* config) {
-    EnableWindow(GetDlgItem(hwndDlg, IDC_PERSIST), config->adapt);
-    EnableWindow(GetDlgItem(hwndDlg, IDC_PERSIST_TITLE), config->adapt);
+static void EnableWindows(HWND hwndDlg, E_AVI* g_this) {
+    bool enable = g_this->config.blend_mode == AVI_BLEND_FIFTY_FIFTY_ONBEAT_ADD;
+    EnableWindow(GetDlgItem(hwndDlg, IDC_PERSIST), enable);
+    EnableWindow(GetDlgItem(hwndDlg, IDC_PERSIST_TITLE), enable);
 }
 
 int win32_dlgproc_avi(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM) {
-    C_THISCLASS* g_ConfigThis = (C_THISCLASS*)g_current_render;
+    E_AVI* g_this = (E_AVI*)g_current_render;
+    const Parameter& p_filename = g_this->info.parameters[0];
+    const Parameter& p_on_beat_persist = g_this->info.parameters[2];
+    const Parameter& p_playback_speed = g_this->info.parameters[3];
+    const Parameter& p_resampling = g_this->info.parameters[4];
+
+    int64_t options_length;
+    const char* const* options;
+
     switch (uMsg) {
-        case WM_INITDIALOG:
-            SendDlgItemMessage(
-                hwndDlg, IDC_PERSIST, TBM_SETRANGE, TRUE, MAKELONG(0, 32));
-            SendDlgItemMessage(
-                hwndDlg, IDC_PERSIST, TBM_SETPOS, TRUE, g_ConfigThis->persist);
+        case WM_INITDIALOG: {
+            auto persist_range =
+                MAKELONG(p_on_beat_persist.int_min, p_on_beat_persist.int_max);
+            SendDlgItemMessage(hwndDlg, IDC_PERSIST, TBM_SETRANGE, TRUE, persist_range);
+            SendDlgItemMessage(hwndDlg,
+                               IDC_PERSIST,
+                               TBM_SETPOS,
+                               TRUE,
+                               g_this->get_int(p_on_beat_persist.handle));
+            auto speed_range =
+                MAKELONG(p_playback_speed.int_min, p_playback_speed.int_max);
             SendDlgItemMessage(hwndDlg, IDC_SPEED, TBM_SETTICFREQ, 50, 0);
-            SendDlgItemMessage(
-                hwndDlg, IDC_SPEED, TBM_SETRANGE, TRUE, MAKELONG(0, 1000));
-            SendDlgItemMessage(
-                hwndDlg, IDC_SPEED, TBM_SETPOS, TRUE, g_ConfigThis->speed);
-            if (g_ConfigThis->enabled) CheckDlgButton(hwndDlg, IDC_CHECK1, BST_CHECKED);
-            if (g_ConfigThis->blend) CheckDlgButton(hwndDlg, IDC_ADDITIVE, BST_CHECKED);
-            if (g_ConfigThis->blendavg) CheckDlgButton(hwndDlg, IDC_5050, BST_CHECKED);
-            if (g_ConfigThis->adapt) CheckDlgButton(hwndDlg, IDC_ADAPT, BST_CHECKED);
-            if (!g_ConfigThis->adapt && !g_ConfigThis->blend
-                && !g_ConfigThis->blendavg) {
-                CheckDlgButton(hwndDlg, IDC_REPLACE, BST_CHECKED);
+            SendDlgItemMessage(hwndDlg, IDC_SPEED, TBM_SETRANGE, TRUE, speed_range);
+            SendDlgItemMessage(hwndDlg,
+                               IDC_SPEED,
+                               TBM_SETPOS,
+                               TRUE,
+                               g_this->get_int(p_playback_speed.handle));
+            if (g_this->enabled) {
+                CheckDlgButton(hwndDlg, IDC_CHECK1, BST_CHECKED);
             }
-            EnableWindows(hwndDlg, g_ConfigThis);
-            loadComboBox(
-                GetDlgItem(hwndDlg, OBJ_COMBO), "*.AVI", g_ConfigThis->ascName);
+            switch (g_this->config.blend_mode) {
+                default:
+                case AVI_BLEND_REPLACE:
+                    CheckDlgButton(hwndDlg, IDC_REPLACE, BST_CHECKED);
+                    break;
+                case AVI_BLEND_ADD:
+                    CheckDlgButton(hwndDlg, IDC_ADDITIVE, BST_CHECKED);
+                    break;
+                case AVI_BLEND_FIFTY_FIFTY:
+                    CheckDlgButton(hwndDlg, IDC_5050, BST_CHECKED);
+                    break;
+                case AVI_BLEND_FIFTY_FIFTY_ONBEAT_ADD:
+                    CheckDlgButton(hwndDlg, IDC_ADAPT, BST_CHECKED);
+                    break;
+            }
+            EnableWindows(hwndDlg, g_this);
+            options = p_filename.get_options(&options_length);
+            for (unsigned int i = 0; i < options_length; i++) {
+                SendDlgItemMessage(
+                    hwndDlg, OBJ_COMBO, CB_ADDSTRING, 0, (LPARAM)options[i]);
+            }
+            int64_t filename_sel = g_this->get_int(p_filename.handle);
+            if (filename_sel >= 0) {
+                SendDlgItemMessage(hwndDlg, OBJ_COMBO, CB_SETCURSEL, filename_sel, 0);
+            }
+            options = p_resampling.get_options(&options_length);
+            for (unsigned int i = 0; i < options_length; i++) {
+                SendDlgItemMessage(
+                    hwndDlg, IDC_AVI_RESAMPLE, CB_ADDSTRING, 0, (LPARAM)options[i]);
+            }
+            int64_t resampling_sel = g_this->get_int(p_resampling.handle);
+            SendDlgItemMessage(
+                hwndDlg, IDC_AVI_RESAMPLE, CB_SETCURSEL, resampling_sel, 0);
+
             return 1;
+        }
         case WM_NOTIFY:
-            if (LOWORD(wParam) == IDC_PERSIST)
-                g_ConfigThis->persist =
+            if (LOWORD(wParam) == IDC_PERSIST) {
+                g_this->config.on_beat_persist =
                     SendDlgItemMessage(hwndDlg, IDC_PERSIST, TBM_GETPOS, 0, 0);
-            if (LOWORD(wParam) == IDC_SPEED)
-                g_ConfigThis->speed =
+            }
+            if (LOWORD(wParam) == IDC_SPEED) {
+                g_this->config.playback_speed =
                     SendDlgItemMessage(hwndDlg, IDC_SPEED, TBM_GETPOS, 0, 0);
+            }
             break;
         case WM_COMMAND:
             if ((LOWORD(wParam) == IDC_CHECK1) || (LOWORD(wParam) == IDC_ADDITIVE)
                 || (LOWORD(wParam) == IDC_REPLACE) || (LOWORD(wParam) == IDC_ADAPT)
                 || (LOWORD(wParam) == IDC_5050)) {
-                g_ConfigThis->enabled = IsDlgButtonChecked(hwndDlg, IDC_CHECK1) ? 1 : 0;
-                g_ConfigThis->blend = IsDlgButtonChecked(hwndDlg, IDC_ADDITIVE) ? 1 : 0;
-                g_ConfigThis->blendavg = IsDlgButtonChecked(hwndDlg, IDC_5050) ? 1 : 0;
-                g_ConfigThis->adapt = IsDlgButtonChecked(hwndDlg, IDC_ADAPT) ? 1 : 0;
-                EnableWindows(hwndDlg, g_ConfigThis);
+                g_this->enabled = IsDlgButtonChecked(hwndDlg, IDC_CHECK1) ? 1 : 0;
+                if (IsDlgButtonChecked(hwndDlg, IDC_ADDITIVE)) {
+                    g_this->config.blend_mode = AVI_BLEND_ADD;
+                } else if (IsDlgButtonChecked(hwndDlg, IDC_5050)) {
+                    g_this->config.blend_mode = AVI_BLEND_FIFTY_FIFTY;
+                } else if (IsDlgButtonChecked(hwndDlg, IDC_ADAPT)) {
+                    g_this->config.blend_mode = AVI_BLEND_FIFTY_FIFTY_ONBEAT_ADD;
+                } else {
+                    g_this->config.blend_mode = AVI_BLEND_REPLACE;
+                }
+                EnableWindows(hwndDlg, g_this);
             }
             // handle clicks to combo box
-            if (HIWORD(wParam) == CBN_SELCHANGE && LOWORD(wParam) == OBJ_COMBO) {
-                int sel = SendDlgItemMessage(hwndDlg, OBJ_COMBO, CB_GETCURSEL, 0, 0);
-                if (sel != -1) {
-                    SendDlgItemMessage(hwndDlg,
-                                       OBJ_COMBO,
-                                       CB_GETLBTEXT,
-                                       sel,
-                                       (LPARAM)g_ConfigThis->ascName);
-                    if (*(g_ConfigThis->ascName))
-                        g_ConfigThis->loadAvi(g_ConfigThis->ascName);
+            if (HIWORD(wParam) == CBN_SELCHANGE) {
+                auto widget = LOWORD(wParam);
+                int64_t sel = SendDlgItemMessage(hwndDlg, widget, CB_GETCURSEL, 0, 0);
+                switch (widget) {
+                    case OBJ_COMBO: {
+                        int64_t old_sel = g_this->get_int(p_filename.handle);
+                        g_this->set_int(p_filename.handle, sel);
+                        if (old_sel != sel) {
+                            g_this->load_file();
+                        }
+                        break;
+                    }
+                    case IDC_AVI_RESAMPLE:
+                        g_this->set_int(p_resampling.handle, sel);
+                        break;
                 }
             }
             return 0;
