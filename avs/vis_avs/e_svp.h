e52344ad84c82a8433632f185162d5643e7294ee
jakob <grandchild@gmx.net>
2022-04-02 14:52:47 +0200

    [port][effect] New-style SVP

---
+++ avs/vis_avs/e_svp.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "r_defs.h"
+
+#include "effect.h"
+#include "effect_info.h"
+#include "svp_vis.h"
+
+#include "../platform.h"
+
+struct SVP_Config : public Effect_Config {
+    int64_t library = -1;
+};
+
+void set_library(Effect*, const Parameter*, std::vector<int64_t>);
+
+struct SVP_Info : public Effect_Info {
+    static constexpr char* group = "Render";
+    static constexpr char* name = "SVP";
+    static constexpr char* help = "";
+    static constexpr int32_t legacy_id = 10;
+    static constexpr char* legacy_ape_id = NULL;
+
+    static const char* const* library_files(int64_t* length_out);
+
+    static constexpr uint32_t num_parameters = 1;
+    static constexpr Parameter parameters[num_parameters] = {
+        P_SELECT(offsetof(SVP_Config, library),
+                 "Library",
+                 library_files,
+                 NULL,
+                 set_library),
+    };
+
+    EFFECT_INFO_GETTERS;
+};
+
+class E_SVP : public Configurable_Effect<SVP_Info, SVP_Config> {
+   public:
+    E_SVP();
+    virtual ~E_SVP();
+    virtual int render(char visdata[2][2][576],
+                       int isBeat,
+                       int* framebuffer,
+                       int* fbout,
+                       int w,
+                       int h);
+    virtual void load_legacy(unsigned char* data, int len);
+    virtual int save_legacy(unsigned char* data);
+
+    void find_library_files();
+    void clear_library_files();
+    void set_library();
+
+    static std::vector<std::string> filenames;
+    static const char** c_filenames;
+
+    dlib_t* library;
+    lock_t* library_lock;
+    VisInfo* vi;
+    VisData vd;
+};
--- avs/vis_avs/g_svp.cpp
+++ avs/vis_avs/g_svp.cpp
@@ -1,4 +1,4 @@
-#include "c_svp.h"
+#include "e_svp.h"
 
 #include "g__defs.h"
 #include "g__lib.h"
@@ -8,31 +8,36 @@
 #include <windows.h>
 
 int win32_dlgproc_svp(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM) {
-    C_THISCLASS* g_this = (C_THISCLASS*)g_current_render;
+    E_SVP* g_this = (E_SVP*)g_current_render;
+    const Parameter& p_library = g_this->info.parameters[0];
+
+    int64_t options_length;
+    const char* const* options;
 
     switch (uMsg) {
-        case WM_INITDIALOG:
-            loadComboBox(GetDlgItem(hwndDlg, IDC_COMBO1), "*.SVP", g_this->m_library);
-            loadComboBox(GetDlgItem(hwndDlg, IDC_COMBO1), "*.UVS", g_this->m_library);
+        case WM_INITDIALOG: {
+            options = p_library.get_options(&options_length);
+            for (unsigned int i = 0; i < options_length; i++) {
+                SendDlgItemMessage(
+                    hwndDlg, IDC_COMBO1, CB_ADDSTRING, 0, (LPARAM)options[i]);
+            }
+            int64_t filename_sel = g_this->get_int(p_library.handle);
+            if (filename_sel >= 0) {
+                SendDlgItemMessage(hwndDlg, IDC_COMBO1, CB_SETCURSEL, filename_sel, 0);
+            }
             return 1;
+        }
         case WM_COMMAND:
             switch (LOWORD(wParam)) {
                 case IDC_COMBO1: {
                     int a = SendDlgItemMessage(hwndDlg, IDC_COMBO1, CB_GETCURSEL, 0, 0);
                     if (a != CB_ERR) {
-                        if (SendDlgItemMessage(hwndDlg,
-                                               IDC_COMBO1,
-                                               CB_GETLBTEXT,
-                                               a,
-                                               (LPARAM)g_this->m_library)
-                            == CB_ERR) {
-                            g_this->m_library[0] = 0;
-                        }
-                    } else
-                        g_this->m_library[0] = 0;
-                    g_this->SetLibrary();
-                }
+                        g_this->set_int(p_library.handle, a);
+                    } else {
+                        g_this->set_int(p_library.handle, -1);
+                    }
                     return 0;
+                }
             }
             return 0;
     }
