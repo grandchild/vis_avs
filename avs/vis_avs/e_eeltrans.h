ed2124be567c131e316197e8a5eb149abb8ef1ab
jakob <grandchild@gmx.net>
2022-04-20 18:43:39 +0200

    [effect] AVSTrans/EELTrans: Use new global config
    Remove all static config members.
    Inline `all_code()` method into the `pre_compile_hook()` method.
    
    The `pre_compile_hook()` implementation still assumes only a single
    AVS instance. The NSEEL compiler needs to be fixed to account for
    multiple AVS instances with distinct pre-/post-compile-hooks.

--- avs/vis_avs/e_eeltrans.h
+++ avs/vis_avs/e_eeltrans.h
@@ -4,14 +4,17 @@
 #include "effect_info.h"
 #include "effect_programmable.h"
 
-#include <set>
-
 struct EelTrans_Config : public Effect_Config {
+    std::string code = "";
+};
+
+struct EelTrans_Global_Config : public Effect_Config {
+    bool translate_enabled = false;
     bool log_enabled = false;
     std::string log_path = "C:\\avslog";
     bool translate_firstlevel = false;
     bool read_comment_codes = true;
-    std::string code = "";
+    bool need_new_first_instance = true;
 };
 
 struct EelTrans_Info : public Effect_Info {
@@ -21,31 +24,25 @@ struct EelTrans_Info : public Effect_Info {
     static constexpr int32_t legacy_id = -1;
     static constexpr char* legacy_ape_id = "Misc: AVSTrans Automation";
 
-    static void apply_global(Effect*, const Parameter*, std::vector<int64_t>);
     static constexpr uint32_t num_parameters = 5;
     static constexpr Parameter parameters[num_parameters] = {
-        P_BOOL(offsetof(EelTrans_Config, log_enabled),
-               "Log Enabled",
-               NULL,
-               apply_global),
-        P_STRING_X(offsetof(EelTrans_Config, log_path), "Log File", NULL, apply_global),
-        P_BOOL(offsetof(EelTrans_Config, translate_firstlevel),
-               "Translate First Level",
-               NULL,
-               apply_global),
-        P_BOOL(offsetof(EelTrans_Config, read_comment_codes),
-               "Read Comment Codes",
-               NULL,
-               apply_global),
-        P_STRING(offsetof(EelTrans_Config, code), "Code", NULL),
+        P_BOOL_G(offsetof(EelTrans_Global_Config, log_enabled), "Log Enabled"),
+        P_STRING_GX(offsetof(EelTrans_Global_Config, log_path), "Log File"),
+        P_BOOL_G(offsetof(EelTrans_Global_Config, translate_firstlevel),
+                 "Translate First Level"),
+        P_BOOL_G(offsetof(EelTrans_Global_Config, read_comment_codes),
+                 "Read Comment Codes"),
+        P_STRING(offsetof(EelTrans_Config, code), "Code"),
     };
 
     EFFECT_INFO_GETTERS;
 };
 
-class E_EelTrans : public Configurable_Effect<EelTrans_Info, EelTrans_Config> {
+class E_EelTrans : public Configurable_Effect<EelTrans_Info,
+                                              EelTrans_Config,
+                                              EelTrans_Global_Config> {
    public:
-    E_EelTrans();
+    E_EelTrans(AVS_Handle avs = 0);
     virtual ~E_EelTrans();
     virtual int render(char visdata[2][2][576],
                        int is_beat,
@@ -59,15 +56,6 @@ class E_EelTrans : public Configurable_Effect<EelTrans_Info, EelTrans_Config> {
 
     static char* pre_compile_hook(void* ctx, char* expression);
     static void post_compile_hook();
-    static std::string all_code();
-
-    static bool translate_enabled;
-    static bool log_enabled;
-    static std::string log_path;
-    static bool translate_firstlevel;
-    static bool read_comment_codes;
-    static bool need_new_first_instance;
-    static std::set<E_EelTrans*> instances;
 
     bool is_first_instance;
 };
