7883d3710a1f22ed4fc8a7f4c2d383fa06022804
exo-cortex <lukipuki@physik.tu-berlin.de>
2022-05-02 16:34:15 +0200

    [port][effect] New-style Grain

--- avs/vis_avs/c_grain.h
+++ avs/vis_avs/c_grain.h
@@ -1,31 +1,59 @@
 #pragma once
 
-#include "c__base.h"
+#include "effect.h"
+#include "effect_info.h"
 
-#define MOD_NAME    "Trans / Grain"
-#define C_THISCLASS C_GrainClass
+struct Grain_Config : public Effect_Config {
+	int64_t blend_mode = 0;
+	int64_t amount = 100;
+	bool static_ = false;
+	
+};
 
-class C_THISCLASS : public C_RBASE {
-   protected:
+struct Grain_Info : public Effect_Info {
+	static constexpr char* group = "Trans";
+	static constexpr char* name = "Grain";
+	static constexpr char* help = "";
+	static constexpr int32_t legacy_id = 24;
+	static constexpr char* legacy_ape_id = NULL;
+
+	static const char* const* blend_modes(int64_t* length_out) {
+		*length_out = 3;
+		static const char* const options[3] = {
+			"Replace",
+			"Additive",
+			"Fifty Fifty",
+		};
+		return options;
+	};
+	
+	static constexpr uint32_t num_parameters = 3;
+	static constexpr Parameter parameters[num_parameters] = {
+		P_SELECT(offsetof(Grain_Config, blend_mode), "Blend Mode", blend_modes),
+		P_IRANGE(offsetof(Grain_Config, amount), "Amount", 0, 100),
+		P_BOOL(offsetof(Grain_Config, static_), "Static"),
+	};
+
+	EFFECT_INFO_GETTERS;
+};
+
+class E_Grain : public Configurable_Effect<Grain_Info, Grain_Config> {
    public:
-    C_THISCLASS();
-    virtual ~C_THISCLASS();
-    virtual int render(char visdata[2][2][576],
-                       int isBeat,
-                       int* framebuffer,
-                       int* fbout,
-                       int w,
-                       int h);
-    virtual char* get_desc() { return MOD_NAME; }
-    virtual void load_config(unsigned char* data, int len);
-    virtual int save_config(unsigned char* data);
-    unsigned char __inline fastrandbyte(void);
-    void reinit(int w, int h);
-    int enabled;
-    int blend, blendavg, smax;
-    unsigned char* depthBuffer;
-    int oldx, oldy;
-    int staticgrain;
-    unsigned char randtab[491];
-    int randtab_pos;
+	E_Grain();
+	virtual ~E_Grain();
+	virtual int render(char visdata[2][2][576],
+					   int is_beat,
+					   int* framebuffer,
+					   int* fbout,
+					   int w,
+					   int h);
+	virtual void load_legacy(unsigned char* data, int len);
+	virtual int save_legacy(unsigned char* data);
+	unsigned char __inline fastrandbyte(void);
+	void reinit(int w, int h);
+	unsigned char* depthBuffer;
+	int oldx;
+	int oldy;
+	unsigned char randtab[491];
+	int randtab_pos;
 };
--- avs/vis_avs/r_grain.cpp
+++ avs/vis_avs/r_grain.cpp
@@ -35,29 +35,35 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <stdlib.h>
 
-C_THISCLASS::~C_THISCLASS()  // set up default configuration
+#define GET_INT() \
+    (data[pos] | (data[pos + 1] << 8) | (data[pos + 2] << 16) | (data[pos + 3] << 24))
+
+#define PUT_INT(y)                   \
+    data[pos] = (y)&255;             \
+    data[pos + 1] = (y >> 8) & 255;  \
+    data[pos + 2] = (y >> 16) & 255; \
+    data[pos + 3] = (y >> 24) & 255
+
+constexpr Parameter Grain_Info::parameters[];
+
+E_Grain::~E_Grain()  // set up default configuration
 {
     if (depthBuffer) free(depthBuffer);
 }
 
 // configuration read/write
 
-C_THISCLASS::C_THISCLASS()  // set up default configuration
+E_Grain::E_Grain()  // set up default configuration
 {
-    blend = 0;
-    blendavg = 0;
-    smax = 100;
-    enabled = 1;
     oldx = 0;
     oldy = 0;
-    staticgrain = 0;
     depthBuffer = NULL;
     unsigned int x;
     for (x = 0; x < sizeof(randtab); x++) randtab[x] = rand() & 255;
     randtab_pos = rand() % sizeof(randtab);
 }
 
-unsigned char __inline C_THISCLASS::fastrandbyte(void) {
+unsigned char inline E_Grain::fastrandbyte(void) {
     unsigned char r = randtab[randtab_pos];
     randtab_pos++;
     if (!(randtab_pos & 15)) {
@@ -67,59 +73,7 @@ unsigned char __inline C_THISCLASS::fastrandbyte(void) {
     return r;
 }
 
-#define GET_INT() \
-    (data[pos] | (data[pos + 1] << 8) | (data[pos + 2] << 16) | (data[pos + 3] << 24))
-void C_THISCLASS::load_config(unsigned char* data, int len)  // read configuration of
-                                                             // max length "len" from
-                                                             // data.
-{
-    int pos = 0;
-    if (len - pos >= 4) {
-        enabled = GET_INT();
-        pos += 4;
-    }
-    if (len - pos >= 4) {
-        blend = GET_INT();
-        pos += 4;
-    }
-    if (len - pos >= 4) {
-        blendavg = GET_INT();
-        pos += 4;
-    }
-    if (len - pos >= 4) {
-        smax = GET_INT();
-        pos += 4;
-    }
-    if (len - pos >= 4) {
-        staticgrain = GET_INT();
-        pos += 4;
-    }
-}
-
-#define PUT_INT(y)                   \
-    data[pos] = (y)&255;             \
-    data[pos + 1] = (y >> 8) & 255;  \
-    data[pos + 2] = (y >> 16) & 255; \
-    data[pos + 3] = (y >> 24) & 255
-int C_THISCLASS::save_config(unsigned char* data)  // write configuration to data,
-                                                   // return length. config data should
-                                                   // not exceed 64k.
-{
-    int pos = 0;
-    PUT_INT(enabled);
-    pos += 4;
-    PUT_INT(blend);
-    pos += 4;
-    PUT_INT(blendavg);
-    pos += 4;
-    PUT_INT(smax);
-    pos += 4;
-    PUT_INT(staticgrain);
-    pos += 4;
-    return pos;
-}
-
-void C_THISCLASS::reinit(int w, int h) {
+void E_Grain::reinit(int w, int h) {
     int x, y;
     unsigned char* p;
     if (depthBuffer) free(depthBuffer);
@@ -133,23 +87,16 @@ void C_THISCLASS::reinit(int w, int h) {
             }
 }
 
-// render function
-// render should return 0 if it only used framebuffer, or 1 if the new output data is in
-// fbout. this is used when you want to do something that you'd otherwise need to make a
-// copy of the framebuffer. w and h are the width and height of the screen, in pixels.
-// isBeat is 1 if a beat has been detected.
-// visdata is in the format of [spectrum:0,wave:1][channel][band].
-
-int C_THISCLASS::render(char[2][2][576],
+int E_Grain::render(char[2][2][576],
                         int isBeat,
                         int* framebuffer,
                         int*,
                         int w,
                         int h) {
     if (isBeat & 0x80000000) return 0;
-    if (!enabled) return 0;
+    if (!this->enabled) return 0;
 
-    int smax_sc = (smax * 255) / 100;
+    int amount_scaled = (this->config.amount * 255) / 100;
     int* p;
     unsigned char* q;
     int l = w * h;
@@ -160,16 +107,16 @@ int C_THISCLASS::render(char[2][2][576],
         oldy = h;
     }
     randtab_pos += rand() % 300;
-    if (randtab_pos >= 491) randtab_pos -= 491;
+    if (randtab_pos >= 491) randtab_pos -= 491; 
 
     p = framebuffer;
     q = depthBuffer;
-    if (staticgrain) {
-        if (blend) {
+    if (this->config.static_) {
+        if (this->config.blend_mode == 1) {
             while (l--) {
                 if (*p) {
                     int c = 0;
-                    if (q[1] < smax_sc) {
+                    if (q[1] < amount_scaled) {
                         int s = q[0];
                         int r = (((p[0] & 0xff0000) * s) >> 8);
                         if (r > 0xff0000) r = 0xff0000;
@@ -186,11 +133,11 @@ int C_THISCLASS::render(char[2][2][576],
                 p++;
                 q += 2;
             }
-        } else if (blendavg) {
+        } else if (this->config.blend_mode == 2) {
             while (l--) {
                 if (*p) {
                     int c = 0;
-                    if (q[1] < smax_sc) {
+                    if (q[1] < amount_scaled) {
                         int s = q[0];
                         int r = (((p[0] & 0xff0000) * s) >> 8);
                         if (r > 0xff0000) r = 0xff0000;
@@ -208,10 +155,11 @@ int C_THISCLASS::render(char[2][2][576],
                 q += 2;
             }
         } else {
+            //  = `replace-mode`
             while (l--) {
                 if (*p) {
                     int c = 0;
-                    if (q[1] < smax_sc) {
+                    if (q[1] < amount_scaled) {
                         int s = q[0];
                         int r = (((p[0] & 0xff0000) * s) >> 8);
                         if (r > 0xff0000) r = 0xff0000;
@@ -230,11 +178,11 @@ int C_THISCLASS::render(char[2][2][576],
             }
         }
     } else {
-        if (blend) {
+        if (this->config.blend_mode == 1) {
             while (l--) {
                 if (*p) {
                     int c = 0;
-                    if (fastrandbyte() < smax_sc) {
+                    if (fastrandbyte() < amount_scaled) {
                         int s = fastrandbyte();
                         int r = (((p[0] & 0xff0000) * s) >> 8);
                         if (r > 0xff0000) r = 0xff0000;
@@ -251,11 +199,11 @@ int C_THISCLASS::render(char[2][2][576],
                 p++;
                 q += 2;
             }
-        } else if (blendavg) {
+        } else if (this->config.blend_mode == 2) {
             while (l--) {
                 if (*p) {
                     int c = 0;
-                    if (fastrandbyte() < smax_sc) {
+                    if (fastrandbyte() < amount_scaled) {
                         int s = fastrandbyte();
                         int r = (((p[0] & 0xff0000) * s) >> 8);
                         if (r > 0xff0000) r = 0xff0000;
@@ -273,10 +221,11 @@ int C_THISCLASS::render(char[2][2][576],
                 q += 2;
             }
         } else {
+            // blendmode `replace`
             while (l--) {
                 if (*p) {
                     int c = 0;
-                    if (fastrandbyte() < smax_sc) {
+                    if (fastrandbyte() < amount_scaled) {
                         int s = fastrandbyte();
                         int r = (((p[0] & 0xff0000) * s) >> 8);
                         if (r > 0xff0000) r = 0xff0000;
@@ -298,12 +247,50 @@ int C_THISCLASS::render(char[2][2][576],
     return 0;
 }
 
-C_RBASE* R_Grain(char* desc)  // creates a new effect object if desc is NULL, otherwise
-                              // fills in desc with description
-{
-    if (desc) {
-        strcpy(desc, MOD_NAME);
-        return NULL;
+void E_Grain::load_legacy(unsigned char* data, int len) {
+    int pos = 0;
+    if (len - pos >= 4) {
+        this->enabled = GET_INT();
+        pos += 4;
     }
-    return (C_RBASE*)new C_THISCLASS();
+    this->config.blend_mode = 0; // "replace"
+    if (len - pos >= 4) {
+        if (GET_INT()) {
+            this->config.blend_mode = 1;
+        }
+        pos += 4;
+    }
+    if (len - pos >= 4) {
+        if (GET_INT() && this->config.blend_mode == 0) {
+            this->config.blend_mode = 2;
+        }
+        pos += 4;
+    }
+    if (len - pos >= 4) {
+        this->config.amount = GET_INT();
+        pos += 4;
+    }
+    if (len - pos >= 4) {
+        this->config.static_ = GET_INT();
+        pos += 4;
+    }
 }
+
+int E_Grain::save_legacy(unsigned char* data) {
+    int pos = 0;
+
+    PUT_INT(this->enabled);
+    pos += 4;
+    PUT_INT(this->config.blend_mode == 1);
+    pos += 4;
+    PUT_INT(this->config.blend_mode == 2);
+    pos += 4;
+    PUT_INT(this->config.amount);
+    pos += 4;
+    PUT_INT(this->config.static_);
+    pos += 4;
+    return pos;
+}
+
+Effect_Info* create_Grain_Info() { return new Grain_Info(); }
+Effect* create_Grain() { return new E_Grain(); }
