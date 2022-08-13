57bd59f1a0430c37ecab9f0777393a23ef19f2ce
jakob <grandchild@gmx.net>
2022-08-13 15:15:44 +0200

    [port][effect] New-style Interleave
    Contains a minor UX change from the original: Moving the on-beat
    duration slider will also set the current x/y values to the on-beat
    values, like moving the on-beat-x/-y always did. This makes checking
    and adjusting the on-beat-duration quicker.

---
+++ avs/vis_avs/e_interleave.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "effect.h"
+#include "effect_common.h"
+#include "effect_info.h"
+
+struct Interleave_Config : public Effect_Config {
+    int64_t x = 1;
+    int64_t y = 1;
+    uint64_t color = 0x000000;
+    bool on_beat = false;
+    int64_t on_beat_x = 1;
+    int64_t on_beat_y = 1;
+    int64_t on_beat_duration = 4;
+    int64_t blend_mode = BLEND_SIMPLE_REPLACE;
+};
+
+struct Interleave_Info : public Effect_Info {
+    static constexpr char* group = "Trans";
+    static constexpr char* name = "Interleave";
+    static constexpr char* help = "";
+    static constexpr int32_t legacy_id = 23;
+    static constexpr char* legacy_ape_id = NULL;
+
+    static void on_x_change(Effect*, const Parameter*, const std::vector<int64_t>&);
+    static void on_y_change(Effect*, const Parameter*, const std::vector<int64_t>&);
+    static void on_duration_change(Effect*,
+                                   const Parameter*,
+                                   const std::vector<int64_t>&);
+
+    static constexpr uint32_t num_parameters = 8;
+    static constexpr Parameter parameters[num_parameters] = {
+        P_IRANGE(offsetof(Interleave_Config, x), "X", 0, 64, NULL, on_x_change),
+        P_IRANGE(offsetof(Interleave_Config, y), "Y", 0, 64, NULL, on_y_change),
+        P_COLOR(offsetof(Interleave_Config, color), "Color"),
+        P_BOOL(offsetof(Interleave_Config, on_beat), "On Beat"),
+        P_IRANGE(offsetof(Interleave_Config, on_beat_x),
+                 "On Beat X",
+                 0,
+                 64,
+                 NULL,
+                 on_x_change),
+        P_IRANGE(offsetof(Interleave_Config, on_beat_y),
+                 "On Beat Y",
+                 0,
+                 64,
+                 NULL,
+                 on_y_change),
+        P_IRANGE(offsetof(Interleave_Config, on_beat_duration),
+                 "On Beat Duration",
+                 0,
+                 64,
+                 NULL,
+                 on_duration_change),
+        P_SELECT(offsetof(Interleave_Config, blend_mode),
+                 "Blend Mode",
+                 blend_modes_simple),
+    };
+
+    EFFECT_INFO_GETTERS;
+};
+
+class E_Interleave : public Configurable_Effect<Interleave_Info, Interleave_Config> {
+   public:
+    E_Interleave();
+
+    virtual ~E_Interleave();
+
+    virtual int render(char visdata[2][2][576],
+                       int is_beat,
+                       int* framebuffer,
+                       int* fbout,
+                       int w,
+                       int h);
+
+    virtual void load_legacy(unsigned char* data, int len);
+
+    virtual int save_legacy(unsigned char* data);
+
+    double cur_x, cur_y;
+};
--- avs/vis_avs/g_interleave.cpp
+++ avs/vis_avs/g_interleave.cpp
@@ -1,6 +1,5 @@
-#include "c_interleave.h"
+#include "e_interleave.h"
 
-#include "g__defs.h"
 #include "g__lib.h"
 
 #include "resource.h"
@@ -9,60 +8,95 @@
 #include <commctrl.h>
 
 int win32_dlgproc_interleave(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    C_THISCLASS* g_ConfigThis = (C_THISCLASS*)g_current_render;
+    auto* g_this = (E_Interleave*)g_current_render;
+    const Parameter& p_x = Interleave_Info::parameters[0];
+    const Parameter& p_y = Interleave_Info::parameters[1];
+    AVS_Parameter_Handle p_color = Interleave_Info::parameters[2].handle;
+    AVS_Parameter_Handle p_on_beat = Interleave_Info::parameters[3].handle;
+    const Parameter& p_on_beat_x = Interleave_Info::parameters[4];
+    const Parameter& p_on_beat_y = Interleave_Info::parameters[5];
+    const Parameter& p_on_beat_duration = Interleave_Info::parameters[6];
+    AVS_Parameter_Handle p_blend_mode = Interleave_Info::parameters[7].handle;
+
     switch (uMsg) {
-        case WM_INITDIALOG:
+        case WM_INITDIALOG: {
+            auto x_range = MAKELONG(p_x.int_min, p_x.int_max);
+            SendDlgItemMessage(hwndDlg, IDC_X, TBM_SETRANGE, TRUE, x_range);
+            auto x = g_this->get_int(p_x.handle);
+            SendDlgItemMessage(hwndDlg, IDC_X, TBM_SETPOS, TRUE, (int32_t)x);
             SendDlgItemMessage(hwndDlg, IDC_X, TBM_SETTICFREQ, 1, 0);
-            SendDlgItemMessage(hwndDlg, IDC_X, TBM_SETRANGE, TRUE, MAKELONG(0, 64));
-            SendDlgItemMessage(hwndDlg, IDC_X, TBM_SETPOS, TRUE, g_ConfigThis->x);
+
+            auto y_range = MAKELONG(p_y.int_min, p_y.int_max);
+            SendDlgItemMessage(hwndDlg, IDC_Y, TBM_SETRANGE, TRUE, y_range);
+            auto y = g_this->get_int(p_y.handle);
+            SendDlgItemMessage(hwndDlg, IDC_Y, TBM_SETPOS, TRUE, (int32_t)y);
             SendDlgItemMessage(hwndDlg, IDC_Y, TBM_SETTICFREQ, 1, 0);
-            SendDlgItemMessage(hwndDlg, IDC_Y, TBM_SETRANGE, TRUE, MAKELONG(0, 64));
-            SendDlgItemMessage(hwndDlg, IDC_Y, TBM_SETPOS, TRUE, g_ConfigThis->y);
+
+            auto on_beat_x_range = MAKELONG(p_on_beat_x.int_min, p_on_beat_x.int_max);
+            SendDlgItemMessage(hwndDlg, IDC_X2, TBM_SETRANGE, TRUE, on_beat_x_range);
+            auto on_beat_x = g_this->get_int(p_on_beat_x.handle);
+            SendDlgItemMessage(hwndDlg, IDC_X2, TBM_SETPOS, TRUE, (int32_t)on_beat_x);
             SendDlgItemMessage(hwndDlg, IDC_X2, TBM_SETTICFREQ, 1, 0);
-            SendDlgItemMessage(hwndDlg, IDC_X2, TBM_SETRANGE, TRUE, MAKELONG(0, 64));
-            SendDlgItemMessage(hwndDlg, IDC_X2, TBM_SETPOS, TRUE, g_ConfigThis->x2);
+
+            auto on_beat_y_range = MAKELONG(p_on_beat_y.int_min, p_on_beat_y.int_max);
+            SendDlgItemMessage(hwndDlg, IDC_Y2, TBM_SETRANGE, TRUE, on_beat_y_range);
+            auto on_beat_y = g_this->get_int(p_on_beat_y.handle);
+            SendDlgItemMessage(hwndDlg, IDC_Y2, TBM_SETPOS, TRUE, (int32_t)on_beat_y);
             SendDlgItemMessage(hwndDlg, IDC_Y2, TBM_SETTICFREQ, 1, 0);
-            SendDlgItemMessage(hwndDlg, IDC_Y2, TBM_SETRANGE, TRUE, MAKELONG(0, 64));
-            SendDlgItemMessage(hwndDlg, IDC_Y2, TBM_SETPOS, TRUE, g_ConfigThis->y2);
-            SendDlgItemMessage(hwndDlg, IDC_X3, TBM_SETTICFREQ, 1, 0);
-            SendDlgItemMessage(hwndDlg, IDC_X3, TBM_SETRANGE, TRUE, MAKELONG(1, 64));
+
+            auto on_beat_duration_range =
+                MAKELONG(p_on_beat_duration.int_min, p_on_beat_duration.int_max);
             SendDlgItemMessage(
-                hwndDlg, IDC_X3, TBM_SETPOS, TRUE, g_ConfigThis->beatdur);
-            if (g_ConfigThis->enabled) CheckDlgButton(hwndDlg, IDC_CHECK1, BST_CHECKED);
-            if (g_ConfigThis->onbeat) CheckDlgButton(hwndDlg, IDC_CHECK8, BST_CHECKED);
-            if (g_ConfigThis->blend) CheckDlgButton(hwndDlg, IDC_ADDITIVE, BST_CHECKED);
-            if (g_ConfigThis->blendavg) CheckDlgButton(hwndDlg, IDC_5050, BST_CHECKED);
-            if (!g_ConfigThis->blend && !g_ConfigThis->blendavg)
-                CheckDlgButton(hwndDlg, IDC_REPLACE, BST_CHECKED);
-            return 1;
-        case WM_HSCROLL: {
-            HWND swnd = (HWND)lParam;
-            int t = (int)SendMessage(swnd, TBM_GETPOS, 0, 0);
-            if (swnd == GetDlgItem(hwndDlg, IDC_X)) {
-                g_ConfigThis->cur_x = (double)(g_ConfigThis->x = t);
-            } else if (swnd == GetDlgItem(hwndDlg, IDC_Y)) {
-                g_ConfigThis->cur_y = (double)(g_ConfigThis->y = t);
-            } else if (swnd == GetDlgItem(hwndDlg, IDC_X2)) {
-                g_ConfigThis->cur_x = (double)(g_ConfigThis->x2 = t);
-            } else if (swnd == GetDlgItem(hwndDlg, IDC_Y2)) {
-                g_ConfigThis->cur_y = (double)(g_ConfigThis->y2 = t);
-            } else if (swnd == GetDlgItem(hwndDlg, IDC_X3)) {
-                g_ConfigThis->beatdur = t;
+                hwndDlg, IDC_X3, TBM_SETRANGE, TRUE, on_beat_duration_range);
+            auto on_beat_duration = g_this->get_int(p_on_beat_duration.handle);
+            SendDlgItemMessage(
+                hwndDlg, IDC_X3, TBM_SETPOS, TRUE, (int32_t)on_beat_duration);
+            SendDlgItemMessage(hwndDlg, IDC_X3, TBM_SETTICFREQ, 1, 0);
+
+            CheckDlgButton(hwndDlg, IDC_CHECK1, g_this->enabled);
+            CheckDlgButton(hwndDlg, IDC_CHECK8, g_this->get_bool(p_on_beat));
+            switch (g_this->get_int(p_blend_mode)) {
+                default:
+                case BLEND_SIMPLE_REPLACE:
+                    CheckDlgButton(hwndDlg, IDC_REPLACE, BST_CHECKED);
+                    break;
+                case BLEND_SIMPLE_ADDITIVE:
+                    CheckDlgButton(hwndDlg, IDC_ADDITIVE, BST_CHECKED);
+                    break;
+                case BLEND_SIMPLE_5050:
+                    CheckDlgButton(hwndDlg, IDC_5050, BST_CHECKED);
+                    break;
             }
+            return 1;
         }
+        case WM_HSCROLL: {
+            if (LOWORD(wParam) == TB_THUMBTRACK) {
+                int64_t slider_value = HIWORD(wParam);
+                if ((HWND)lParam == GetDlgItem(hwndDlg, IDC_X)) {
+                    g_this->set_int(p_x.handle, slider_value);
+                } else if ((HWND)lParam == GetDlgItem(hwndDlg, IDC_Y)) {
+                    g_this->set_int(p_y.handle, slider_value);
+                } else if ((HWND)lParam == GetDlgItem(hwndDlg, IDC_X2)) {
+                    g_this->set_int(p_on_beat_x.handle, slider_value);
+                } else if ((HWND)lParam == GetDlgItem(hwndDlg, IDC_Y2)) {
+                    g_this->set_int(p_on_beat_y.handle, slider_value);
+                } else if ((HWND)lParam == GetDlgItem(hwndDlg, IDC_X3)) {
+                    g_this->set_int(p_on_beat_duration.handle, slider_value);
+                }
+            }
             return 0;
+        }
         case WM_DRAWITEM: {
-            DRAWITEMSTRUCT* di = (DRAWITEMSTRUCT*)lParam;
-            if (di->CtlID == IDC_DEFCOL)  // paint nifty color button
-            {
-                unsigned int _color = g_ConfigThis->color;
-                _color = ((_color >> 16) & 0xff) | (_color & 0xff00)
-                         | ((_color << 16) & 0xff0000);
+            auto* di = (DRAWITEMSTRUCT*)lParam;
+            if (di->CtlID == IDC_DEFCOL) {
+                unsigned int color = g_this->get_color(p_color);
+                color = ((color >> 16) & 0xff) | (color & 0xff00)
+                        | ((color << 16) & 0xff0000);
 
                 HPEN hPen, hOldPen;
                 HBRUSH hBrush, hOldBrush;
-                LOGBRUSH lb = {BS_SOLID, _color, 0};
-                hPen = (HPEN)CreatePen(PS_SOLID, 0, _color);
+                LOGBRUSH lb = {BS_SOLID, color, 0};
+                hPen = (HPEN)CreatePen(PS_SOLID, 0, color);
                 hBrush = CreateBrushIndirect(&lb);
                 hOldPen = (HPEN)SelectObject(di->hDC, hPen);
                 hOldBrush = (HBRUSH)SelectObject(di->hDC, hBrush);
@@ -76,36 +110,47 @@ int win32_dlgproc_interleave(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lPar
                 DeleteObject(hBrush);
                 DeleteObject(hPen);
             }
-        }
             return 0;
-        case WM_COMMAND:
-            if ((LOWORD(wParam) == IDC_CHECK1) || (LOWORD(wParam) == IDC_CHECK8)
-                || (LOWORD(wParam) == IDC_ADDITIVE) || (LOWORD(wParam) == IDC_REPLACE)
-                || (LOWORD(wParam) == IDC_5050)) {
-                g_ConfigThis->enabled = IsDlgButtonChecked(hwndDlg, IDC_CHECK1) ? 1 : 0;
-                g_ConfigThis->onbeat = IsDlgButtonChecked(hwndDlg, IDC_CHECK8) ? 1 : 0;
-                g_ConfigThis->blend = IsDlgButtonChecked(hwndDlg, IDC_ADDITIVE) ? 1 : 0;
-                g_ConfigThis->blendavg = IsDlgButtonChecked(hwndDlg, IDC_5050) ? 1 : 0;
+        }
+        case WM_COMMAND: {
+            auto element = LOWORD(wParam);
+            if ((element == IDC_CHECK1) || (element == IDC_CHECK8)
+                || (element == IDC_ADDITIVE) || (element == IDC_REPLACE)
+                || (element == IDC_5050)) {
+                g_this->set_enabled(IsDlgButtonChecked(hwndDlg, IDC_CHECK1));
+                g_this->set_bool(p_on_beat, IsDlgButtonChecked(hwndDlg, IDC_CHECK8));
+                if (element == IDC_REPLACE || element == IDC_ADDITIVE
+                    || element == IDC_5050) {
+                    if (IsDlgButtonChecked(hwndDlg, IDC_REPLACE)) {
+                        g_this->set_int(p_blend_mode, BLEND_SIMPLE_REPLACE);
+                    } else if (IsDlgButtonChecked(hwndDlg, IDC_ADDITIVE)) {
+                        g_this->set_int(p_blend_mode, BLEND_SIMPLE_ADDITIVE);
+                    } else if (IsDlgButtonChecked(hwndDlg, IDC_5050)) {
+                        g_this->set_int(p_blend_mode, BLEND_SIMPLE_5050);
+                    }
+                }
             }
-            if (LOWORD(wParam) == IDC_DEFCOL)  // handle clicks to nifty color button
-            {
-                int* a = &(g_ConfigThis->color);
+            if (LOWORD(wParam) == IDC_DEFCOL) {
+                unsigned int color = g_this->get_color(p_color);
                 static COLORREF custcolors[16];
                 CHOOSECOLOR cs;
                 cs.lStructSize = sizeof(cs);
                 cs.hwndOwner = hwndDlg;
-                cs.hInstance = 0;
-                cs.rgbResult =
-                    ((*a >> 16) & 0xff) | (*a & 0xff00) | ((*a << 16) & 0xff0000);
+                cs.hInstance = nullptr;
+                cs.rgbResult = ((color >> 16) & 0xff) | (color & 0xff00)
+                               | ((color << 16) & 0xff0000);
                 cs.lpCustColors = custcolors;
                 cs.Flags = CC_RGBINIT | CC_FULLOPEN;
                 if (ChooseColor(&cs)) {
-                    *a = ((cs.rgbResult >> 16) & 0xff) | (cs.rgbResult & 0xff00)
-                         | ((cs.rgbResult << 16) & 0xff0000);
-                    g_ConfigThis->color = *a;
+                    color = ((cs.rgbResult >> 16) & 0xff) | (cs.rgbResult & 0xff00)
+                            | ((cs.rgbResult << 16) & 0xff0000);
+                    g_this->set_color(p_color, color);
                 }
-                InvalidateRect(GetDlgItem(hwndDlg, IDC_DEFCOL), NULL, TRUE);
+                InvalidateRect(GetDlgItem(hwndDlg, IDC_DEFCOL), nullptr, TRUE);
             }
+            return 0;
+        }
+        default: break;
     }
     return 0;
 }
