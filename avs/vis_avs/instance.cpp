d18998b54ab3e72f598d31506af761ac3623e1e7
jakob <grandchild@mailbox.org>
2023-10-02 22:33:55 +0200

    [port] Copy file loading & buffers to AVS_Instance
    Updated code from EffectList & rlib.cpp.
    
    This is still inert, but will be used after the following commits.

--- avs/vis_avs/instance.cpp
+++ avs/vis_avs/instance.cpp
@@ -1,11 +1,21 @@
-#include "instance.h"
+#include "e_root.h"
 
-#include <stdio.h>
+#include "effect_library.h"
+#include "instance.h"
+#include "pixel_format.h"
+
+#include <cstdio>
 
 AVS_Instance::AVS_Instance(AVS_Audio_Source audio_source, AVS_Beat_Source beat_source)
-    : audio_source(audio_source), beat_source(beat_source), error("") {}
-
-AVS_Instance::~AVS_Instance(){};
+    : audio_source(audio_source),
+      beat_source(beat_source),
+      error(""),
+      render_lock(lock_init()) {
+    for (auto& pixel_format : this->buffers_pixel_format) {
+        pixel_format = AVS_PIXEL_RGB0_8;
+    }
+    make_effect_lib();
+}
 
 bool AVS_Instance::render_frame(void* framebuffer,
                                 uint64_t time_ms,
@@ -15,26 +25,149 @@ bool AVS_Instance::render_frame(void* framebuffer,
                                 AVS_Pixel_Format pixel_format) {
     return true;
 }
+
 bool AVS_Instance::audio_set(int16_t* audio_left,
                              int16_t* audio_right,
                              size_t audio_length,
                              size_t samples_per_second) {
     return this->audio_source == AVS_AUDIO_EXTERNAL;
 }
+
 int32_t AVS_Instance::audio_device_count() { return 1; }
+
 const char* const* AVS_Instance::audio_device_names() { return this->audio_devices; }
-void AVS_Instance::audio_device_set(int32_t device) { return; }
-bool AVS_Instance::preset_load_file(const char* file_path) { return true; }
-bool AVS_Instance::preset_set(char* preset) { return true; }
-bool AVS_Instance::preset_set_legacy(uint8_t* preset, size_t preset_length) {
+
+void AVS_Instance::audio_device_set(int32_t device) {}
+
+bool AVS_Instance::preset_load_file(const char* file_path, bool with_transition) {
+    bool success = false;
+    FILE* fp = fopen(file_path, "rb");
+    if (fp != nullptr) {
+        fseek(fp, 0, SEEK_END);
+        size_t len = ftell(fp);
+        fseek(fp, 0, SEEK_SET);
+        if (len > MAX_LEGACY_PRESET_FILESIZE_BYTES) {
+            len = MAX_LEGACY_PRESET_FILESIZE_BYTES;
+        }
+        auto data = (uint8_t*)calloc(len, 1);
+        if (!fread(data, 1, len, fp)) {
+            len = 0;
+        }
+        fclose(fp);
+        auto file_magic_length = strlen(AVS_Instance::legacy_file_magic);
+        if (len > file_magic_length + 2
+            && !memcmp(data, AVS_Instance::legacy_file_magic, file_magic_length - 2)
+            && data[file_magic_length - 2] >= '1' && data[file_magic_length - 2] <= '2'
+            && data[file_magic_length - 1] == '\x1a') {
+            success = this->preset_load_legacy(
+                data + file_magic_length, len - file_magic_length, with_transition);
+        } else {
+            auto preset_string = std::string((char*)data, len);
+            success = this->preset_load(preset_string, with_transition);
+        }
+        free(data);
+    }
+    return success;
+}
+
+bool AVS_Instance::preset_load(std::string& preset, bool with_transition) {
     return true;
 }
-void AVS_Instance::preset_save(const char* file_path, bool indent) { return; }
-void AVS_Instance::preset_save_legacy(const char* file_path) { return; }
-const char* AVS_Instance::preset_get() { return ""; }
+
+bool AVS_Instance::preset_load_legacy(uint8_t* preset,
+                                      size_t preset_length,
+                                      bool with_transition) {
+    E_Root& load_root = with_transition ? this->root_secondary : this->root;
+    lock_lock(this->render_lock);
+    load_root = E_Root();
+    load_root.load_legacy(preset, (int)(preset_length));
+    lock_unlock(this->render_lock);
+    return true;
+}
+
+void AVS_Instance::preset_save_file(const char* file_path, bool indent) {}
+
+int AVS_Instance::preset_save_file_legacy(const char* file_path) {
+    int result = -1;
+    auto data = (uint8_t*)calloc(MAX_LEGACY_PRESET_FILESIZE_BYTES, 1);
+    if (data) {
+        auto file_magic_length = strlen(AVS_Instance::legacy_file_magic);
+        memcpy(data, AVS_Instance::legacy_file_magic, file_magic_length);
+        size_t size = this->preset_save_legacy(data + file_magic_length);
+        size += file_magic_length;
+        if (size < MAX_LEGACY_PRESET_FILESIZE_BYTES) {
+            FILE* fp = fopen(file_path, "wb");
+            if (fp != nullptr) {
+                result = 0;
+                fwrite(data, 1, size, fp);
+                fclose(fp);
+            } else {
+                result = 2;
+            }
+        } else {
+            result = 1;
+        }
+        free(data);
+    }
+    return result;
+}
+
+const char* AVS_Instance::preset_save() { return ""; }
+
+size_t AVS_Instance::preset_save_legacy(uint8_t* data, bool secondary) {
+    int success = -1;
+    size_t pos = 0;
+    if (data) {
+        lock_lock(this->render_lock);
+        if (secondary) {
+            pos += this->root_secondary.save_legacy(data);
+        } else {
+            pos += this->root.save_legacy(data);
+        }
+    }
+    lock_unlock(this->render_lock);
+    return pos;
+}
+
 void AVS_Instance::clear() { this->root = E_Root(); }
+void AVS_Instance::clear_secondary() { this->root_secondary = E_Root(); }
+
 const char* AVS_Instance::error_str() { return this->error; }
+
+bool AVS_Instance::undo() { return false; }
+bool AVS_Instance::redo() { return false; }
 
 Effect* AVS_Instance::get_component_from_handle(AVS_Component_Handle component) {
     return this->root.find_by_handle(component);
+}
+
+void* AVS_Instance::get_buffer(size_t w,
+                               size_t h,
+                               int32_t buffer_num,
+                               bool allocate_if_needed) {
+    if (buffer_num < 0 || buffer_num >= NBUF) {
+        return nullptr;
+    }
+    if (w == 0 || h == 0) {
+        return nullptr;
+    }
+    void*& buffer = this->buffers[buffer_num];
+    size_t& buffer_w = this->buffers_w[buffer_num];
+    size_t& buffer_h = this->buffers_h[buffer_num];
+    if (buffer == nullptr || buffer_w < w || buffer_h < h) {
+        if (buffer != nullptr) {
+            free(buffer);
+        }
+        if (allocate_if_needed) {
+            buffer = calloc(w * h, pixel_size(this->buffers_pixel_format[buffer_num]));
+            buffer_w = w;
+            buffer_h = h;
+        } else {
+            buffer = nullptr;
+            buffer_w = 0;
+            buffer_h = 0;
+        }
+        return buffer;
+    }
+    return buffer;
 }
--- avs/vis_avs/instance.h
+++ avs/vis_avs/instance.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "c_transition.h"
 #include "e_root.h"
 
 #include "avs.h"
@@ -7,10 +8,16 @@
 #include "effect.h"
 #include "effect_info.h"
 
+#include "../platform.h"
+
+#include <string>
+
+#define MAX_LEGACY_PRESET_FILESIZE_BYTES (1024 * 1024)
+
 class AVS_Instance {
    public:
     AVS_Instance(AVS_Audio_Source audio_source, AVS_Beat_Source beat_source);
-    ~AVS_Instance();
+    ~AVS_Instance() = default;
 
     bool render_frame(void* framebuffer,
                       uint64_t time_ms,
@@ -25,17 +32,29 @@ class AVS_Instance {
     int32_t audio_device_count();
     const char* const* audio_device_names();
     void audio_device_set(int32_t device);
-    bool preset_load_file(const char* file_path);
-    bool preset_set(char* preset);
-    bool preset_set_legacy(uint8_t* preset, size_t preset_length);
-    void preset_save(const char* file_path, bool indent);
-    void preset_save_legacy(const char* file_path);
-    const char* preset_get();
+    bool preset_load_file(const char* file_path, bool with_transition = false);
+    bool preset_load(std::string& preset, bool with_transition = false);
+    bool preset_load_legacy(uint8_t* preset,
+                            size_t preset_length,
+                            bool with_transition = false);
+    void preset_save_file(const char* file_path, bool indent);
+    int preset_save_file_legacy(const char* file_path);
+    /** Allocate 1 MiB of memory at `data`. Return value is the actual size. */
+    size_t preset_save_legacy(uint8_t* data, bool secondary = false);
+    const char* preset_save();
     void clear();
+    void clear_secondary();
     const char* error_str();
+    bool undo();
+    bool redo();
 
     Effect_Info* get_effect_from_handle(AVS_Effect_Handle effect);
     Effect* get_component_from_handle(AVS_Component_Handle component);
+
+    void* get_buffer(size_t w,
+                     size_t h,
+                     int32_t buffer_num,
+                     bool allocate_if_needed = false);
 
     AVS_Audio_Source audio_source;
     AVS_Beat_Source beat_source;
@@ -43,5 +62,17 @@ class AVS_Instance {
     const char* audio_devices[1] = {""};
 
     E_Root root;
+    /** Used for transitioning between presets. */
+    E_Root root_secondary;
     std::vector<Effect*> scrap;
+
+    lock_t* render_lock;
+
+   private:
+    static constexpr char const* legacy_file_magic = "Nullsoft AVS Preset 0.2\x1a";
+    static constexpr size_t num_buffers = 8;
+    void* buffers[num_buffers];
+    size_t buffers_w[num_buffers];
+    size_t buffers_h[num_buffers];
+    AVS_Pixel_Format buffers_pixel_format[num_buffers];
 };
