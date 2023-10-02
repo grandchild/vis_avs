9ba2ae2ee840e3a966d7df885dc0f738a3912160
jakob <grandchild@mailbox.org>
2023-10-02 21:35:10 +0200

    [bug] Ensure timer resolution inited before usage
    And remove underscore "_" from symbols leading with it, since it's
    reserved. Make timer_us_init() static instead.

--- avs/platform_win32.c
+++ avs/platform_win32.c
@@ -6,29 +6,34 @@
 
 /* timers */
 
-static int64_t _timer_highres_ticks_per_us = 0;
+static int64_t timer_highres_ticks_per_us = 0;
 
-void _timer_us_init() {
+static void timer_us_init() {
     LARGE_INTEGER ticks_per_second = {.QuadPart = 0};
     if (!QueryPerformanceFrequency(&ticks_per_second)) {
-        _timer_highres_ticks_per_us = 1;
+        timer_highres_ticks_per_us = 1;
     } else {
-        _timer_highres_ticks_per_us = ticks_per_second.QuadPart / (1000 * 1000);
+        timer_highres_ticks_per_us = ticks_per_second.QuadPart / (1000 * 1000);
     }
 }
 
 uint64_t timer_ms() { return GetTickCount64(); }
 
 uint64_t timer_us() {
-    if (_timer_highres_ticks_per_us == 0) {
-        _timer_us_init();
+    if (timer_highres_ticks_per_us == 0) {
+        timer_us_init();
     }
     LARGE_INTEGER result;
     QueryPerformanceCounter(&result);
-    return result.QuadPart / _timer_highres_ticks_per_us;
+    return result.QuadPart / timer_highres_ticks_per_us;
 }
 
-double timer_us_precision() { return 1.0f / (double)_timer_highres_ticks_per_us; }
+double timer_us_precision() {
+    if (timer_highres_ticks_per_us == 0) {
+        timer_us_init();
+    }
+    return 1.0f / (double)timer_highres_ticks_per_us;
+}
 
 /* locking */
 
