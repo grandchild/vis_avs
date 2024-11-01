5beb4747a45a507e95e68d814b38f2d9150b0e8d
jakob <grandchild@mailbox.org>
2024-11-01 23:46:01 +0100

    [util][build] Add UUID generation methods
    Links to libuuid on Linux, which comes with util-linux. On Windows it
    uses rpc.h's UuidCreate method.
    
    UUIDs will be used as globally unique preset identifiers.

---
+++ avs/uuid.cpp
@@ -0,0 +1,24 @@
+#include "uuid.h"
+
+#ifdef _WIN32
+#include <rpc.h>
+#elif defined __linux__
+#include <uuid/uuid.h>
+#endif
+
+std::string uuid4() {
+    auto uuid_str = (char*)malloc(37);
+    uuid_str[36] = '\0';
+#ifdef _WIN32
+    UUID uuid;
+    UuidCreate(&uuid);
+    UuidToString(&uuid, (unsigned char**)&uuid_str);
+#elif defined __linux__
+    uuid_t uuid;
+    uuid_generate(uuid);
+    uuid_unparse_lower(uuid, uuid_str);
+#endif
+    auto retval = std::string(uuid_str);
+    free(uuid_str);
+    return retval;
+}
