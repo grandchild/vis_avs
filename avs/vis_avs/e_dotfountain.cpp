ff9adea4e8a53bf3250515b4af6c17916d0b8ef6
jakob <grandchild@mailbox.org>
2024-12-24 14:59:08 +0100

    [refactor] Get PI from constants.h for DotFountain

--- avs/vis_avs/e_dotfountain.cpp
+++ avs/vis_avs/e_dotfountain.cpp
@@ -32,9 +32,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 // alphachannel safe 11/21/99
 #include "e_dotfountain.h"
 
-#include "r_defs.h"
-
 #include "blend.h"
+#include "constants.h"
 #include "matrix.h"
 
 #include <array>
