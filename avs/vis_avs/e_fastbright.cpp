9e0884a25b32972a236ba4529c65d55c7bb83219
jakob <grandchild@mailbox.org>
2025-07-19 08:28:23 +0200

    [port] Replace FastBrightness asm with intrinsics

--- avs/vis_avs/e_fastbright.cpp
+++ avs/vis_avs/e_fastbright.cpp
@@ -31,6 +31,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #include "e_fastbright.h"
 
+#include <immintrin.h>
+
 #define GET_INT() \
     (data[pos] | (data[pos + 1] << 8) | (data[pos + 2] << 16) | (data[pos + 3] << 24))
 #define PUT_INT(y)                   \
@@ -42,7 +44,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 constexpr Parameter FastBright_Info::parameters[];
 
 E_FastBright::E_FastBright(AVS_Instance* avs) : Configurable_Effect(avs) {
-#ifdef NO_MMX
+#ifndef SIMD_MODE_X86_SSE
     int x;
     for (x = 0; x < 128; x++) {
         tab[0][x] = x + x;
@@ -68,8 +70,8 @@ int E_FastBright::render(char[2][2][576],
     if (is_beat & 0x80000000) {
         return 0;
     }
-#ifdef NO_MMX
-    // the non mmx x2 version really isn't in any terms faster than normal brightness
+#ifndef SIMD_MODE_X86_SSE
+    // the non simd version really isn't in any terms faster than normal brightness
     // with no exclusions turned on
     {
         unsigned int* t = (unsigned int*)framebuffer;
@@ -100,190 +102,26 @@ int E_FastBright::render(char[2][2][576],
         }
     }
 #else
-    int mask[2] = {
-        0x7F7F7F7F,
-        0x7F7F7F7F,
-    };
-    int l = (w * h);
-    if (this->config.multiply == FASTBRIGHT_MULTIPLY_DOUBLE) {
-#ifdef _MSC_VER  // MSVC asm
-        __asm {
-			mov edx, l
-			mov edi, framebuffer
-      shr edx, 3  // 8 pixels at a time
-      align 16
-		fastbright_l1:
-			movq mm0, [edi]
-			movq mm1, [edi+8]
-			movq mm2, [edi+16]
-      paddusb mm0, mm0
-			movq mm3, [edi+24]
-      paddusb mm1, mm1
-      paddusb mm2, mm2
-      movq [edi], mm0
-      paddusb mm3, mm3
-      movq [edi+8], mm1
-      movq [edi+16], mm2
-      movq [edi+24], mm3
-
-      add edi, 32
-
-			dec edx
-			jnz fastbright_l1
-
-			mov edx, l
-			and edx, 7
-      shr edx, 1  // up the last 7 pixels (two at a time)
-			jz fastbright_l3
-
-		fastbright_l2:
-			movq mm3, [edi]
-      paddusb mm3, mm3
-      movq [edi], mm3
-			add edi, 8
-			dec edx
-			jnz fastbright_l2
-		fastbright_l3:
-			emms
+    size_t len = w * h;
+    switch (this->config.multiply) {
+        case FASTBRIGHT_MULTIPLY_DOUBLE:
+            for (size_t i = 0; i < len; i += 4) {
+                __m128i fb_4px = _mm_loadu_si128((__m128i*)&framebuffer[i]);
+                fb_4px = _mm_adds_epu8(fb_4px, fb_4px);
+                _mm_storeu_si128((__m128i*)&framebuffer[i], fb_4px);
+            }
+            break;
+        case FASTBRIGHT_MULTIPLY_HALF: {
+            const __m128i mask = _mm_set1_epi32(0x7F7F7F7F);
+            for (size_t i = 0; i < len; i += 4) {
+                __m128i fb_4px = _mm_loadu_si128((__m128i*)&framebuffer[i]);
+                fb_4px = _mm_srli_epi16(fb_4px, 1);
+                fb_4px = _mm_and_si128(fb_4px, mask);
+                _mm_storeu_si128((__m128i*)&framebuffer[i], fb_4px);
+            }
+            break;
         }
-#else            // _MSC_VER, GCC asm
-        __asm__ __volatile__(
-            "mov     %%edx, %[l]\n\t"
-            "mov     %%edi, %[framebuffer]\n\t"
-            "shr     %%edx, 3\n\t"
-            ".align  16\n"
-            "fastbright_l1:\n\t"
-            "movq    %%mm0, [%%edi]\n\t"
-            "movq    %%mm1, [%%edi + 8]\n\t"
-            "movq    %%mm2, [%%edi + 16]\n\t"
-            "paddusb %%mm0, %%mm0\n\t"
-            "movq    %%mm3, [%%edi+24]\n\t"
-            "paddusb %%mm1, %%mm1\n\t"
-            "paddusb %%mm2, %%mm2\n\t"
-            "movq    [%%edi], %%mm0\n\t"
-            "paddusb %%mm3, %%mm3\n\t"
-            "movq    [%%edi + 8], %%mm1\n\t"
-            "movq    [%%edi + 16], %%mm2\n\t"
-            "movq    [%%edi + 24], %%mm3\n\t"
-            "add     %%edi, 32\n\t"
-            "dec     %%edx\n\t"
-            "jnz     fastbright_l1\n\t"
-            "mov     %%edx, %[l]\n\t"
-            "and     %%edx, 7\n\t"
-            "shr     %%edx, 1\n\t"
-            "jz      fastbright_l3\n"
-            "fastbright_l2:\n\t"
-            "movq    %%mm3, [%%edi]\n\t"
-            "paddusb %%mm3, %%mm3\n\t"
-            "movq    [%%edi], %%mm3\n\t"
-            "add     %%edi, 8\n\t"
-            "dec     %%edx\n\t"
-            "jnz     fastbright_l2\n"
-            "fastbright_l3:\n\t"
-            "emms\n\t"
-            : /* no outputs */
-            : [l] "m"(l), [framebuffer] "m"(framebuffer)
-            : "edx", "esi");
-#endif           // _MSC_VER
-    } else if (this->config.multiply == FASTBRIGHT_MULTIPLY_HALF) {
-#ifdef _MSC_VER  // MSVC asm
-        __asm {
-			mov edx, l
-      movq mm7, [mask]
-			mov edi, framebuffer
-      shr edx, 3  // 8 pixels at a time
-      align 16
-		_lr1:
-			movq mm0, [edi]
-			movq mm1, [edi+8]
-
-			movq mm2, [edi+16]
-      psrl mm0, 1
-
-			movq mm3, [edi+24]
-      pand mm0, mm7
-      
-      psrl mm1, 1
-      movq [edi], mm0
-
-      psrl mm2, 1
-      pand mm1, mm7
-
-      movq [edi+8], mm1
-      pand mm2, mm7
-
-      psrl mm3, 1
-      movq [edi+16], mm2
-
-      pand mm3, mm7
-      movq [edi+24], mm3
-
-      add edi, 32
-
-			dec edx
-			jnz _lr1
-
-			mov edx, l
-			and edx, 7
-      shr edx, 1  // up the last 7 pixels (two at a time)
-			jz _lr3
-
-		_lr2:
-			movq mm3, [edi]
-      psrl mm3, 1
-      pand mm3, mm7
-      movq [edi], mm3
-			add edi, 8
-			dec edx
-			jnz _lr2
-		_lr3:
-			emms
-        }
-#else            // _MSC_VER, GCC asm
-        __asm__ __volatile__(
-            "mov   %%edx, %[l]\n\t"
-            "movq  %%mm7, [%[mask]]\n\t"
-            "mov   %%edi, %[framebuffer]\n\t"
-            "shr   %%edx, 3\n\t"
-            ".align 16\n"
-            "_lr1:\n\t"
-            "movq  %%mm0, [%%edi]\n\t"
-            "movq  %%mm1, [%%edi + 8]\n\t"
-            "movq  %%mm2, [%%edi + 16]\n\t"
-            "psrlq %%mm0, 1\n\t"
-            "movq  %%mm3, [%%edi + 24]\n\t"
-            "pand  %%mm0, %%mm7\n\t"
-            "psrlq %%mm1, 1\n\t"
-            "movq  [%%edi], %%mm0\n\t"
-            "psrlq %%mm2, 1\n\t"
-            "pand  %%mm1, %%mm7\n\t"
-            "movq  [%%edi + 8], %%mm1\n\t"
-            "pand  %%mm2, %%mm7\n\t"
-            "psrlq %%mm3, 1\n\t"
-            "movq  [%%edi + 16], %%mm2\n\t"
-            "pand  %%mm3, %%mm7\n\t"
-            "movq  [%%edi + 24], %%mm3\n\t"
-            "add   %%edi, 32\n\t"
-            "dec   %%edx\n\t"
-            "jnz   _lr1\n\t"
-            "mov   %%edx, %[l]\n\t"
-            "and   %%edx, 7\n\t"
-            "shr   %%edx, 1\n\t"
-            "jz    _lr3\n"
-            "_lr2:\n\t"
-            "movq  %%mm3, [%%edi]\n\t"
-            "psrlq %%mm3, 1\n\t"
-            "pand  %%mm3, %%mm7\n\t"
-            "movq  [%%edi], %%mm3\n\t"
-            "add   %%edi, 8\n\t"
-            "dec   %%edx\n\t"
-            "jnz   _lr2\n"
-            "_lr3:\n\t"
-            "emms\n\t"
-            :
-            : [l] "m"(l), [mask] "m"(mask), [framebuffer] "m"(framebuffer)
-            : "edx", "edi");
-#endif           // _MSC_VER
+        default: break;
     }
 #endif
     return 0;
